657a53b09d1e0a07c0d4e2e54766d2e9b6129967
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:38:05 +0300

    Add Movie Ticket Pricing
    Added code to calculate movie ticket prices based on the type of movie ("Premiere," "Normal," or "Discount") and the number of rows and seats. Prices are set at $12.0 per seat for "Premiere," $7.50 per seat for "Normal," and $5.00 per seat for "Discount." The total price is displayed with two decimal places.

---
+++ 02.ComplexConditionalStatements/09.Cinema/Cinema.cs
@@ -0,0 +1,31 @@
+﻿namespace _09.Cinema
+{
+    internal class Cinema
+    {
+        static void Main(string[] args)
+        {
+            string typeOfMovie = Console.ReadLine();
+            int countOfRows = int.Parse(Console.ReadLine());
+            int countOfSeats = int.Parse(Console.ReadLine());
+
+            int totalCountOfSeats = countOfRows * countOfSeats;
+            double price = 0;
+
+            if (typeOfMovie == "Premiere")
+            {
+                price = totalCountOfSeats * 12.0;
+            }
+            else if (typeOfMovie == "Normal")
+            {
+                price = totalCountOfSeats * 7.50;
+            }
+            else if (typeOfMovie == "Discount")
+            {
+                price = totalCountOfSeats * 5.00;
+            }
+
+            Console.WriteLine($"{price:f2}");
+
+        }
+    }
+}
