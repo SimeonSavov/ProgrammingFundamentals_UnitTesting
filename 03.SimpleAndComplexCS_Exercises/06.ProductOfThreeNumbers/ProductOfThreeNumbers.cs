1a647869f0128f0332fb16693db783224c688c3c
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:33:49 +0300

    Add Multi-Number Sign Evaluation
    Added code to determine the overall sign (positive, negative, or zero) based on three user-input integers (num1, num2, num3). The program uses a series of conditional statements to assess the signs of the numbers individually and then determine the overall sign. This update enhances the program's ability to classify the combined sign of multiple numbers without performing multiplication or arithmetic operations.

---
+++ 03.SimpleAndComplexCS_Exercises/06.ProductOfThreeNumbers/ProductOfThreeNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+namespace _06.ProductOfThreeNumbers
+{
+    internal class ProductOfThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+                Console.WriteLine("zero");
+            else if (num1 > 0 && num2 > 0 && num3 > 0)
+                Console.WriteLine("positive");
+            else if (num1 > 0 && num2 > 0 && num3 < 0)
+                Console.WriteLine("negative");
+            else if (num1 > 0 && num2 < 0 && num3 > 0)
+                Console.WriteLine("negative");
+            else if (num1 > 0 && num2 < 0 && num3 < 0)
+                Console.WriteLine("positive");
+            else if (num1 < 0 && num2 > 0 && num3 > 0)
+                Console.WriteLine("negative");
+            else if (num1 < 0 && num2 > 0 && num3 < 0)
+                Console.WriteLine("positive");
+            else if (num1 < 0 && num2 < 0 && num3 > 0)
+                Console.WriteLine("positive");
+            else if (num1 < 0 && num2 < 0 && num3 < 0)
+                Console.WriteLine("negative");
+        }
+    }
+}
