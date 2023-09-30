e0803ec307da93059642230a21d867fdca580566
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:34:41 +0300

    Add Vowel or Consonant Recognition
    Implemented a program that recognizes whether a character input by the user is a vowel or a consonant. The switch-case statement handles both uppercase and lowercase versions of the English vowels (A, a, E, e, I, i, O, o, U, u). If the input is not a vowel, it is classified as a consonant. This update enhances the program's ability to categorize characters.

---
+++ 02.ComplexConditionalStatements/07.VowelOrConsonant/VowelOrConsonant.cs
@@ -0,0 +1,31 @@
+﻿namespace _07.VowelOrConsonant
+{
+    internal class VowelOrConsonant
+    {
+        static void Main(string[] args)
+        {
+            string character = Console.ReadLine();
+
+            switch (character)
+            {
+                case "A":
+                case "a":
+                case "E":
+                case "e":
+                case "I":
+                case "i":
+                case "O":
+                case "o":
+                case "U":
+                case "u":
+                    Console.WriteLine("Vowel");
+                    break;
+                default:
+                    Console.WriteLine("Consonant");
+                    break;
+
+            }
+
+        }
+    }
+}
