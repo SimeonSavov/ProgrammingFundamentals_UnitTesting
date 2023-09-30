6f8932909a58dab592af1890cf5b7a744699179e
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:19:29 +0300

    Add drink and extra price checker with validation
    This commit extends the previous drink and extra price calculator by adding validation checks. It calculates the final price of a drink and optional extra while ensuring valid selections for both drink and extra options. If an unknown drink or extra is chosen, the program displays an error message and sets the 'isValid' flag to false, only proceeding to calculate and display the final price when all selections are valid.

---
+++ 01.ConditionalStatements_Lab/12.CoffeeShopWithChecks/CoffeeShopWithChecks.cs
@@ -0,0 +1,51 @@
+﻿namespace _12.CoffeeShopWithChecks
+{
+    internal class CoffeeShopWithChecks
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string extra = Console.ReadLine();
+
+            double price = 0;
+            bool isValid = true;
+
+            if (drink == "coffee")
+            {
+                price += 1;
+            }
+            else if (drink == "tea")
+            {
+                price += 0.6;
+            }
+            else
+            {
+                Console.WriteLine("Unknown drink");
+                isValid = false;
+            }
+
+            if (isValid)
+            {
+                if (extra == "sugar")
+                {
+                    price += 0.4;
+                }
+                else if (extra == "no")
+                {
+                    price += 0.0;
+                }
+                else
+                {
+                    Console.WriteLine("Unknown extra");
+                    isValid = false;
+                }
+
+                if (isValid )
+                {
+                    Console.WriteLine($"Final price: ${price:f2}");
+                }
+            }
+            
+        }
+    }
+}
