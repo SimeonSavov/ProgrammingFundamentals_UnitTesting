0aa1c7966d85c470068ae68a86bf91cf389ead6f
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:07:42 +0300

    Add water boiling temperature check
    This commit adds a temperature check for water. It prompts the user for a temperature input and checks whether the temperature is greater than 100 degrees. If the temperature is above 100 degrees, it displays "The water is boiling"; otherwise, it shows "The water is not hot enough." This change enhances the program by providing feedback on the water temperature.

---
+++ 01.ConditionalStatements_Lab/06.BoilingWater/BoilingWater.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _06.BoilingWater
+{
+    internal class BoilingWater
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number > 100)
+            {
+                Console.WriteLine("The water is boiling");
+            }
+            else
+            {
+                Console.WriteLine("The water is not hot enough");
+            }
+
+
+        }
+    }
+}
