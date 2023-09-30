36b611c566a5502b11cb40dc68576b37c428c1e5
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:16:18 +0300

    Add drink and extra price calculator
    This commit introduces a program that calculates the final price of a drink based on user choices for "coffee" or "tea" and whether they want to add "sugar" as an extra. It uses if-else statements to determine the base price for the selected drink and adds an extra cost for sugar if chosen. The final price is then displayed with a precision of two decimal places.

---
+++ 01.ConditionalStatements_Lab/11.CoffeeShop/CoffeeShop.cs
@@ -0,0 +1,29 @@
+﻿namespace _11.CoffeeShop
+{
+    internal class CoffeeShop
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string extra = Console.ReadLine();
+
+            double price = 0;
+
+            if (drink == "coffee")
+            {
+                price = 1;
+            }
+            else if (drink == "tea")
+            {
+                price = 0.6;
+            }
+
+            if (extra == "sugar")
+            {
+                price += 0.4;
+            }
+
+            Console.WriteLine($"Final price: ${price:f2}");
+        }
+    }
+}
