968632819960af0614c85316e86b2a1c15abfe50
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:25:40 +0300

    Add Bonus Points Calculation
    Added code to calculate bonus points based on user input (points) and display the result. Bonus points are determined by input ranges: 0-3, 4-6, and 7-9, corresponding to 5, 15, and 20 points, respectively. This enhances program functionality.

---
+++ 02.ComplexConditionalStatements/03.BonusScore/BonusScore.cs
@@ -0,0 +1,28 @@
+﻿namespace _03.BonusScore
+{
+    internal class BonusScore
+    {
+        static void Main(string[] args)
+        {
+            int points = int.Parse(Console.ReadLine());
+
+            int bonus = 0;
+
+            if (points >= 0 && points <= 3)
+            {
+                bonus = 5;
+            }
+            else if (points >= 4 && points <= 6)
+            {
+                bonus = 15;
+            }
+            else if (points >= 7 && points <= 9)
+            {
+                bonus = 20;
+            }
+
+            int totalSum = bonus + points;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
