36e038b15ef3751ff1fc48004f3ff2042716b26c
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:30:15 +0300

    Add Basic Calculator
    Implemented a basic calculator that performs arithmetic operations (addition, subtraction, multiplication, and division) on two user-input numbers (num1 and num2) based on the operator (op) provided as input. The result is displayed with two decimal places for precision. This update enhances the program's functionality to perform basic mathematical calculations.

---
+++ 03.SimpleAndComplexCS_Exercises/04.NumberOperations/NumberOperations.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.XPath;
+
+namespace _04.NumberOperations
+{
+    internal class NumberOperations
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            string op = Console.ReadLine();
+
+            double result = 0;
+
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+            Console.WriteLine($"{num1} {op} {num2} = {result:f2}");
+
+        }
+    }
+}
