914237930b4711aac2870e68430a492f2ecd6b5e
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:37:55 +0300

    Add Time Calculation and Formatting
    Added code to calculate the total time of three athletes' performances (athlete1Time, athlete2Time, athlete3Time) in seconds and format it as minutes and seconds (mm:ss). The program calculates the total time, converts it to minutes and seconds, and ensures that single-digit seconds are displayed with leading zeros for consistency. This update enhances the program's functionality for time calculation and formatting.

---
+++ 03.SimpleAndComplexCS_Exercises/09.SumSeconds/SumSeconds.cs
@@ -0,0 +1,26 @@
+﻿namespace _09.SumSeconds
+{
+    internal class SumSeconds
+    {
+        static void Main(string[] args)
+        {
+            int athlete1Time = int.Parse(Console.ReadLine());
+            int athlete2Time = int.Parse(Console.ReadLine());
+            int athlete3Time = int.Parse(Console.ReadLine());
+
+            int totalTime = athlete1Time + athlete2Time + athlete3Time;
+
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+        }
+    }
+}
