70be5c4f4259f926eafde9ecfee9ae52c21b9763
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:29:51 +0300

    Implement Product Categorization (Fruits and Vegetables)
    Added code to categorize user input (product) into "fruit," "vegetable," or "unknown" categories. If the input matches common fruits like banana, apple, kiwi, cherry, or lemon, it's labeled as "fruit." Similarly, if it matches vegetables like cucumber, pepper, or carrot, it's labeled as "vegetable." If the input doesn't match either category, it's marked as "unknown." This update follows a similar logic pattern to previous tasks, enhancing the program's ability to categorize products based on user input.

---
+++ 02.ComplexConditionalStatements/05.FruitOrVegetable/FruitOrVegetable.cs
@@ -0,0 +1,23 @@
+﻿namespace _05.FruitOrVegetable
+{
+    internal class FruitOrVegetable
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+
+            if (product == "banana" || product == "apple" || product == "kiwi" || product == "cherry" || product == "lemon")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (product == "cucumber" || product == "pepper" || product == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
