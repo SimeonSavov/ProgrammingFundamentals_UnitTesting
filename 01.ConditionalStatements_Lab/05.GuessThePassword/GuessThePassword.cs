7955c73a399ad4bbbae57b217a671f7b0dccd895
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:06:23 +0300

    Implement password authentication
    This commit introduces a basic password authentication system. It prompts the user for a password input and checks if it matches the expected password "s3cr3t!". If the input password is correct, it displays "Welcome"; otherwise, it shows "Wrong password!" to the user. This change enhances the program by adding a simple authentication mechanism.

---
+++ 01.ConditionalStatements_Lab/05.GuessThePassword/GuessThePassword.cs
@@ -0,0 +1,19 @@
+﻿namespace _05.GuessThePassword
+{
+    internal class GuessThePassword
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            if (password == "s3cr3t!")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
