f152a7811ae362598484f870aa5cae8aac94f265
Simeon <simeonsavov21@gmail.com>
2023-09-30 08:54:10 +0300

    Add even/odd number check
    This commit adds a condition to check if the input number is even or odd. If the number is even, it will print "even"; otherwise, it will print "odd." This change enhances the functionality of the program by providing feedback on the parity of the input number.

---
+++ 01.ConditionalStatements_Lab/02.EvenOrOdd/EvenOrOdd.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.EvenOrOdd
+{
+    internal class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
