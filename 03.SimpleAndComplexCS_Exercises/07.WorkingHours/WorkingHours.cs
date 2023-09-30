994a7ebde91fbdf915f822ee1174160aa765b570
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:35:29 +0300

    Add Business Hour Status Checker
    Added code to check the operating hours of a business based on the day of the week (day) and the hour of the day (hourOfTheDay). If it's Sunday, the business is marked as "closed" regardless of the time. On other days, it evaluates whether the hour falls within the range of 10:00 AM to 6:00 PM and accordingly determines if the business is "open" or "closed." This update enhances the program's ability to assess business hours.

---
+++ 03.SimpleAndComplexCS_Exercises/07.WorkingHours/WorkingHours.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.WorkingHours
+{
+    internal class WorkingHours
+    {
+        static void Main(string[] args)
+        {
+            int hourOfTheDay = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (day == "Sunday")
+            {
+                Console.WriteLine("closed");
+            }
+            else
+            {
+                if (hourOfTheDay >= 10 && hourOfTheDay <= 18)
+                {
+                    Console.WriteLine("open");
+                }
+                else
+                {
+                    Console.WriteLine("closed");
+                }
+            }
+
+        }
+    }
+}
