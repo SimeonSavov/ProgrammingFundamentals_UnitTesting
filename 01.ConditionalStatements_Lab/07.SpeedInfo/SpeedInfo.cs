1080ae58cda2a9ce8f86d707f50930ad64cf005c
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:09:11 +0300

    Add speed assessment
    This commit adds a basic speed evaluation feature. It takes a speed input and outputs "Fast" if the speed is greater than 30, and "Slow" otherwise.

---
+++ 01.ConditionalStatements_Lab/07.SpeedInfo/SpeedInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace _07.SpeedInfo
+{
+    internal class SpeedInfo
+    {
+        static void Main(string[] args)
+        {
+            double speed = double.Parse(Console.ReadLine());
+
+            if (speed > 30)
+            {
+                Console.WriteLine("Fast");
+            }
+            else
+            {
+                Console.WriteLine("Slow");
+            }
+        }
+    }
+}
