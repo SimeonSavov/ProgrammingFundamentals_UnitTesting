aa96feabd6752ad8e8fcce51fda20eca3647b754
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:13:09 +0300

    Add geometric figure area calculator
    This commit introduces a program that calculates the area of geometric figures. It utilizes nested if-else-if statements to handle different figure types ("square," "rectangle," or "circle") and computes the corresponding area based on the provided dimensions. If an invalid figure type is entered, it displays "Invalid figure."

---
+++ 01.ConditionalStatements_Lab/09.AreaOfFigures/AreaOfFigures.cs
@@ -0,0 +1,34 @@
+﻿namespace _09.AreaOfFigures
+{
+    internal class AreaOfFigures
+    {
+        static void Main(string[] args)
+        {
+            string figureType = Console.ReadLine();
+
+            if (figureType == "square")
+            {
+                double size = double.Parse(Console.ReadLine());
+                double area = size * size;
+                Console.WriteLine($"{area:f2}");
+            }
+            else if (figureType == "rectangle")
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                double num2 = double.Parse(Console.ReadLine());
+                double area = num1 * num2;
+                Console.WriteLine($"{area:f2}");
+            }
+            else if (figureType == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double area = Math.PI * Math.Pow(radius, 2);
+                Console.WriteLine($"{area:f2}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid figure.");
+            }
+        }
+    }
+}
