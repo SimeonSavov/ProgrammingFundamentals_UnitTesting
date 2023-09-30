a3fd79f0b709218911c6514dfd8b821a274d2c3a
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:23:33 +0300

    Add Max Number Comparison
    Implemented code to find and display the maximum number among three user-input integers (num1, num2, num3). The code uses nested if-else statements for straightforward comparison and output.

---
+++ 02.ComplexConditionalStatements/02.LargestNumberOutOfThree/LargestNumberOutOfThree.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.LargestNumberOutOfThree
+{
+    internal class LargestNumberOutOfThree
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3) 
+                {
+                    Console.WriteLine(num1);
+                }
+                else
+                {
+                    Console.WriteLine(num3);
+                }
+            }
+            else if (num1 < num2)
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine(num2);
+                }
+                else
+                {
+                    Console.WriteLine(num3);
+                }
+            }
+        }
+    }
+}
