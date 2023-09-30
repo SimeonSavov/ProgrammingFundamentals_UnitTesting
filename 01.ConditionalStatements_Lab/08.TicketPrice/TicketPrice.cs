8aff1222f3a7750cb1006c64801577caa60ec939
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:10:20 +0300

    Add ticket price calculator
    This commit introduces a basic ticket price calculator. It prompts the user for a ticket type (either "student" or "regular") and calculates and displays the corresponding ticket price. If an invalid ticket type is provided, it displays "Invalid ticket type!"

---
+++ 01.ConditionalStatements_Lab/08.TicketPrice/TicketPrice.cs
@@ -0,0 +1,26 @@
+﻿namespace _08.TicketPrice
+{
+    internal class TicketPrice
+    {
+        static void Main(string[] args)
+        {
+            string ticketType = Console.ReadLine();
+            double price = 0;
+
+            if (ticketType == "student")
+            {
+                price = 1;
+                Console.WriteLine($"${price:f2}");
+            }
+            else if (ticketType == "regular")
+            {
+                price = 1.6;
+                Console.WriteLine($"${price:f2}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid ticket type!");
+            }
+        }
+    }
+}
