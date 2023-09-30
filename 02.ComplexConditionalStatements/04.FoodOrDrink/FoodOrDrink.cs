9e606f7881bceda5c9f7b404ef34e75bc3e0a16e
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:28:28 +0300

    Implement Product Categorization
    Added code to categorize user input (product) into "food," "drink," or "unknown" categories. If the input matches common food items like curry, noodles, sushi, spaghetti, or bread, it's labeled as "food." Similarly, if it matches beverages like tea, water, coffee, or juice, it's labeled as "drink." If the input doesn't match either category, it's marked as "unknown." This update enhances the program's ability to categorize products.

---
+++ 02.ComplexConditionalStatements/04.FoodOrDrink/FoodOrDrink.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.FoodOrDrink
+{
+    internal class FoodOrDrink
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+
+            if (product == "curry" || product == "noodles" || product == "sushi" || product == "spaghetti" || product == "bread")
+            {
+                Console.WriteLine("food");
+            }
+            else if (product == "tea" || product == "water" || product == "coffee" || product == "juice")
+            {
+                Console.WriteLine("drink");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
