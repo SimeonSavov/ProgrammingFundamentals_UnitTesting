9d7da10a6b735a4131d9d908c1667d94dc4523ff
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:35:46 +0300

    Implement Number Sorting Recognition
    Added code to determine the sorting order of three user-input numbers (num1, num2, num3). If the numbers are in ascending order (num1 < num2 < num3), it displays "Ascending." If they are in descending order (num1 > num2 > num3), it displays "Descending." Otherwise, it indicates "Not sorted." This update enhances the program's ability to recognize the order of numbers.

---
+++ 02.ComplexConditionalStatements/08.SortedNumbers/SortedNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace _08.SortedNumbers
+{
+    internal class SortedNumbers
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 < num2 && num2 < num3)
+            {
+                Console.WriteLine("Ascending");
+            }
+            else if (num1 > num2 && num2 > num3)
+            {
+                Console.WriteLine("Descending");
+            }
+            else
+            {
+                Console.WriteLine("Not sorted");
+            }
+            
+        }
+    }
+}
