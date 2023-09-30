2e6cfc3de33f5683dd2e6c9716e4ff79ba62c8a0
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:25:02 +0300

    Add Number Sign Determination
    Implemented code to determine the sign of an integer input (number) and display "negative" if it's less than zero, "zero" if it's equal to zero, or "positive" if it's greater than zero. This update enhances the program's ability to classify numbers based on their sign.

---
+++ 03.SimpleAndComplexCS_Exercises/01.NumberType/NumberType.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.NumberType
+{
+    internal class NumberType
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0 )
+            {
+                Console.WriteLine("negative");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else
+            {
+                Console.WriteLine("positive");
+            }
+        }
+    }
+}
