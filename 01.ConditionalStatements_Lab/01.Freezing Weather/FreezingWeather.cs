2b99bb5587f073a5dcd74ce279e1d55eab096d72
Simeon <simeonsavov21@gmail.com>
2023-09-30 08:52:01 +0300

    Add temperature condition in Celsius
    This commit introduces a temperature condition in Celsius. If the input temperature in Celsius is less than or equal to zero, it will display "Freezing weather!" as a message. This change was made to handle cold weather conditions in the program.

---
+++ 01.ConditionalStatements_Lab/01.Freezing Weather/FreezingWeather.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.Freezing_Weather
+{
+    internal class FreezingWeather
+    {
+        static void Main(string[] args)
+        {
+            double celsius = double.Parse(Console.ReadLine());
+
+            if (celsius <= 0)
+            {
+                Console.WriteLine("Freezing weather!");
+            }
+        }
+    }
+}
