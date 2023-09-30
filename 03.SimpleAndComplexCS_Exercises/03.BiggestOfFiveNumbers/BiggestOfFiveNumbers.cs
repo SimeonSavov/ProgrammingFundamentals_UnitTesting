88d57342e2e3137e7ab93228fa673abe6597d037
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:29:13 +0300

    Add Maximum Number Detection
    Added code to find and display the maximum number among five user-input integers (num1, num2, num3, num4, num5). The program uses a series of conditional statements to compare each number and determine the largest one. This update enhances the program's ability to identify the maximum value among multiple inputs.

---
+++ 03.SimpleAndComplexCS_Exercises/03.BiggestOfFiveNumbers/BiggestOfFiveNumbers.cs
@@ -0,0 +1,36 @@
+﻿namespace _03.BiggestOfFiveNumbers
+{
+    internal class BiggestOfFiveNumbers
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            int num5 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5)
+            {
+                Console.WriteLine(num1);
+            }
+            else if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5)
+            {
+                Console.WriteLine(num2);
+            }
+            else if (num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5)
+            {
+                Console.WriteLine(num3);
+            }
+            else if (num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5)
+            {
+                Console.WriteLine(num4);
+            }
+            else if (num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4)
+            {
+                Console.WriteLine(num5);
+            }
+        }
+
+    }
+}
