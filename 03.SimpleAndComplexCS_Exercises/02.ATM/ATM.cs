0dd6602cd84cffda48df24ea740b7cba4dcc7bba
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:26:47 +0300

    Add Withdrawal Limit Check
    Implemented code to check if a withdrawal request (withdraw) is within the available balance (balance) and a specified limit (limit). If the withdrawal is less than or equal to the balance, it's considered successful. If it exceeds the limit, it indicates that the limit was exceeded. Otherwise, it reports "Insufficient availability." This update enhances the program's ability to handle withdrawal transactions.

---
+++ 03.SimpleAndComplexCS_Exercises/02.ATM/ATM.cs
@@ -0,0 +1,25 @@
+﻿namespace _02.ATM
+{
+    internal class ATM
+    {
+        static void Main(string[] args)
+        {
+            int balance = int.Parse(Console.ReadLine());
+            int withdraw = int.Parse(Console.ReadLine());
+            int limit = int.Parse(Console.ReadLine());
+
+            if (withdraw <= balance)
+            {
+                Console.WriteLine("The withdraw was successful.");
+            }
+            else if (withdraw > limit)
+            {
+                Console.WriteLine("The limit was exceeded.");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient availability.");
+            }
+        }
+    }
+}
