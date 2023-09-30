6f5e646a18924e6a84fab5c4b47fd4aa992f0efb
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:38:51 +0300

    Add Clothing and Shoes Recommendation
    Implemented code to provide clothing and shoes recommendations based on the temperature (temp) and the time of day (dayTime). The program considers various temperature and time scenarios to suggest appropriate attire and footwear. It then generates a message to inform the user about what to wear. This update enhances the program's functionality to offer clothing and shoes recommendations.

---
+++ 03.SimpleAndComplexCS_Exercises/10.SummerOutfit/SummerOutfit.cs
@@ -0,0 +1,40 @@
+﻿namespace _10.SummerOutfit
+{
+    internal class SummerOutfit
+    {
+        static void Main(string[] args)
+        {
+            int temp = int.Parse(Console.ReadLine());
+            string dayTime = Console.ReadLine();
+
+            string shoes = "";
+            string clothing = "";
+
+            if ((dayTime == "Evening")
+                || (dayTime == "Afternoon" && temp <= 18)
+                || (dayTime == "Morning" && temp > 18 && temp <= 24))
+            {
+                clothing = "Shirt";
+                shoes = "Moccasins";
+            }
+            else if ((dayTime == "Morning" && temp >= 25) 
+                || (dayTime == "Afternoon" && temp > 18 && temp <= 24))
+            {
+                clothing = "T-Shirt";
+                shoes = "Sandals";
+            }
+            else if (dayTime == "Morning" && temp <= 18)
+            {
+                clothing = "Sweatshirt";
+                shoes = "Sneakers";
+            }
+            else if (dayTime == "Afternoon" && temp >= 25)
+            {
+                clothing = "Swim Suit";
+                shoes = "Barefoot";
+            }
+
+            Console.WriteLine($"It's {temp} degrees, get your {clothing} and {shoes}.");
+        }
+    }
+}
