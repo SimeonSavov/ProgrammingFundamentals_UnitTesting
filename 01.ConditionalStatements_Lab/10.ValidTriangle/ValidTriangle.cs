d7e5fa04b17d80e1874e9bed6033f91298e24cc7
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:14:33 +0300

    Implement triangle validity checker
    This commit introduces a program that checks the validity of a triangle based on the lengths of its sides. It uses a series of if-else-if statements to evaluate whether the input side lengths (num1, num2, and num3) can form a valid triangle. If the conditions for a valid triangle are met, it displays "Valid Triangle"; otherwise, it outputs "Invalid Triangle."

---
+++ 01.ConditionalStatements_Lab/10.ValidTriangle/ValidTriangle.cs
@@ -0,0 +1,29 @@
+﻿namespace _10.ValidTriangle
+{
+    internal class ValidTriangle
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+
+            if (num1 + num2 <= num3)
+            {
+                Console.WriteLine("Invalid Triangle");
+            }
+            else if (num1 + num3 <= num2)
+            {
+                Console.WriteLine("Invalid Triangle");
+            }
+            else if (num2 + num3 <= num1)
+            {
+                Console.WriteLine("Invalid Triangle");
+            }
+            else
+            {
+                Console.WriteLine("Valid Triangle");
+            }
+        }
+    }
+}
