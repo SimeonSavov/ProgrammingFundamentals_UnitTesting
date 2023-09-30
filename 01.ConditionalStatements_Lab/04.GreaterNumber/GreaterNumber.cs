2d8036aebf0289af1767003a8e13522c9e8bbf05
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:03:50 +0300

    Add greater number comparison
    This commit adds a comparison between two integer inputs, `number1` and `number2`, to determine which one is greater. It then displays the greater number along with the message "Greater number." This change enhances the program by providing a way to compare and identify the larger of the two input numbers.

---
+++ 01.ConditionalStatements_Lab/04.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace _04.GreaterNumber
+{
+    internal class GreaterNumber
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 > number2)
+            {
+                Console.WriteLine($"Greater number: {number1}");
+            }
+            else
+            {
+                Console.WriteLine($"Greater number: {number2}");
+            }
+        }
+    }
+}
