acb41425881a15347b640a477551ee47a7bbd4e0
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:31:22 +0300

    Add Seasonal Vacation Expenses Calculator
    Implemented code to calculate vacation expenses based on the season (season), accommodation type (accType), and the number of days (countOfDays). The program adjusts prices for different seasons and accommodation types, applying discounts or surcharges as needed. The final total expenses are displayed with two decimal places for precision. This update enhances the program's functionality to calculate vacation costs.

---
+++ 03.SimpleAndComplexCS_Exercises/05.VacationExpenses/VacationExpenses.cs
@@ -0,0 +1,66 @@
+﻿namespace _05.VacationExpenses
+{
+    internal class VacationExpenses
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            string accType = Console.ReadLine();
+            int countOfDays = int.Parse(Console.ReadLine());
+
+            double totalExpenses = 0;
+
+            if (season == "Spring")
+            {
+                if (accType == "Hotel")
+                {
+                    totalExpenses = countOfDays * 30;
+                    
+                }
+                else if (accType == "Camping")
+                {
+                    totalExpenses = countOfDays * 10;
+                    
+                }
+                totalExpenses *= 0.8;
+            }
+            else if (season == "Summer")
+            {
+                if ( accType == "Hotel")
+                {
+                    totalExpenses = countOfDays * 50;
+                }
+                else if (accType == "Camping")
+                {
+                    totalExpenses = countOfDays * 30;
+                }
+            }
+            else if (season == "Autumn")
+            {
+                if (accType == "Hotel")
+                {
+                    totalExpenses = countOfDays * 20;
+                }
+                else if (accType == "Camping")
+                {
+                    totalExpenses = countOfDays * 15;
+                }
+                totalExpenses *= 0.7;
+            }
+            else if (season == "Winter")
+            {
+                if (accType == "Hotel")
+                {
+                    totalExpenses = countOfDays * 40;
+                }
+                else if (accType == "Camping")
+                {
+                    totalExpenses = countOfDays * 10;
+                }
+                totalExpenses *= 0.9;
+            }
+
+            Console.WriteLine($"{totalExpenses:f2}");
+        }
+    }
+}
