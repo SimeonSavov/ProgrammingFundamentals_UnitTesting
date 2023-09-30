022a7c870b9a7f3410dfc1f24e64155cbef6c17d
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:22:05 +0300

    Add Complex Pricing Logic
    Added code to handle complex pricing logic for different products and weekdays. The program now calculates and displays prices based on user input.

---
+++ 02.ComplexConditionalStatements/01.MarketPlace/MarketPlace.cs
@@ -0,0 +1,47 @@
+﻿namespace _01.MarketPlace
+{
+    internal class MarketPlace
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string day = Console.ReadLine();
+
+            if (product == "Banana")
+            {
+                if (day == "Weekday")
+                {
+                    Console.WriteLine("2.50");
+                }
+                else
+                {
+                    Console.WriteLine("2.70");
+                }
+            }
+            else if (product == "Apple")
+            {
+                if (day == "Weekday")
+                {
+                    Console.WriteLine("1.30");
+                }
+                else
+                {
+                    Console.WriteLine("1.60");
+                }
+            }
+            else if (product == "Kiwi")
+            {
+                if (day == "Weekday")
+                {
+                    Console.WriteLine("2.20");
+                }
+                else
+                {
+                    Console.WriteLine("3.00");
+                }
+            }
+
+            
+        }
+    }
+}
