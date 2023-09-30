eba2044a4daeddacaf8d93867d13d55f1a9cb889
Simeon <simeonsavov21@gmail.com>
2023-09-30 09:01:44 +0300

    Implement number-to-word converter
    This commit introduces a number-to-word converter using a switch-case logic. It takes an integer input and converts it into its corresponding word representation (e.g., 1 becomes "one," 5 becomes "five"). Additionally, it handles out-of-range inputs by displaying "Out of range." This change enhances the program's functionality by providing a user-friendly way to represent numeric values as words.

---
+++ 01.ConditionalStatements_Lab/03.Number1To9AsWords/Number1To9AsWords.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.Number1To9AsWords
+{
+    internal class Number1To9AsWords
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1: Console.WriteLine("one"); break;
+                case 2: Console.WriteLine("two"); break;
+                case 3: Console.WriteLine("three"); break;
+                case 4: Console.WriteLine("four"); break;
+                case 5: Console.WriteLine("five"); break;
+                case 6: Console.WriteLine("six"); break;
+                case 7: Console.WriteLine("seven"); break;
+                case 8: Console.WriteLine("eight"); break;
+                case 9: Console.WriteLine("nine"); break;
+                default: Console.WriteLine("Out of range"); break;
+            }
+        }
+    }
+}
