a75e64d19afe4cb1dc2758cb9a2eb6986e523055
Simeon <simeonsavov21@gmail.com>
2023-10-04 08:12:14 +0300

    Add countdown to exam day program
    This commit adds a C# program that counts down the days before an exam. Users input the number of days remaining, and the program displays a daily countdown message in the format "{i} days before the exam," where 'i' represents the remaining days. Finally, it announces "The exam has come."

---
+++ 04.ForLoop_Lab/06.ExamCountdown/ExamCountdown.cs
@@ -0,0 +1,17 @@
+﻿namespace _06.ExamCountdown
+{
+    internal class ExamCountdown
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            for (int i = day; i > 0; i--)
+            {
+                Console.WriteLine($"{i} days before the exam");
+            }
+
+            Console.WriteLine("The exam has come");
+        }
+    }
+}
