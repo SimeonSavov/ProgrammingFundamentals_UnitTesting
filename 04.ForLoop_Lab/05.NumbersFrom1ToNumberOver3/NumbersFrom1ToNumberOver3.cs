533daac7d5d049d9b1d9f4855dcab66eedc524d9
Simeon <simeonsavov21@gmail.com>
2023-10-04 08:10:30 +0300

    Add number printing program
    This code adds a C# program that prints numbers in increments of 3, starting from 1 and going up to 'n'. The user is prompted to input a positive integer 'n', and then the program displays numbers in the sequence 1, 4, 7, etc., until 'n' is reached.

---
+++ 04.ForLoop_Lab/05.NumbersFrom1ToNumberOver3/NumbersFrom1ToNumberOver3.cs
@@ -0,0 +1,15 @@
+﻿namespace _05.NumbersFrom1ToNumberOver3
+{
+    internal class NumbersFrom1ToNumberOver3
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
