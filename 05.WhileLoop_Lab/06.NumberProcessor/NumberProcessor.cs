2f2110cc306ceb26d9a141a93003d9c28e46e4e2
Simeon <simeonsavov21@gmail.com>
2023-10-04 07:35:52 +0300

    Add program for numeric manipulation with user-defined commands
    This commit introduces a program that allows users to manipulate a numeric value using custom commands. The program starts with an initial integer input and then enters a loop where the user can provide commands, such as "Inc" to increment the number or "Dec" to decrement it. The loop continues until the user inputs "End," at which point the final manipulated value is displayed.

---
+++ 05.WhileLoop_Lab/06.NumberProcessor/NumberProcessor.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.NumberProcessor
+{
+    internal class NumberProcessor
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                if (command == "Inc")
+                {
+                    num++;
+                }
+                else if (command == "Dec")
+                {
+                    num--;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(num);
+        }
+    }
+}
