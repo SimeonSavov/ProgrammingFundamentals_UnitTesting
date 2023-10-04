5598b74cd59c3d7bf354443555d4583a9e23cdd9
Simeon <simeonsavov21@gmail.com>
2023-10-04 08:13:07 +0300

    Add character range printing program
    This commit adds a C# program that prints characters between two user-provided characters, 'letter1' and 'letter2', inclusive. Users input two characters, and the program displays the characters within that range, separated by spaces.

---
+++ 04.ForLoop_Lab/07.LatinLetters/LatinLetters.cs
@@ -0,0 +1,16 @@
+﻿namespace _07.LatinLetters
+{
+    internal class LatinLetters
+    {
+        static void Main(string[] args)
+        {
+            char letter1 = char.Parse(Console.ReadLine());
+            char letter2 = char.Parse(Console.ReadLine());
+
+            for (char ch = letter1; ch <= letter2; ch++)
+            {
+                Console.Write($"{ch} ");
+            }
+        }
+    }
+}
