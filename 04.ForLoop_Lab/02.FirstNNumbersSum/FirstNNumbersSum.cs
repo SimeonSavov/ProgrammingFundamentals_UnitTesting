cb55424f3ad67b9a4d5ba703a7644525ff15b491
Simeon <simeonsavov21@gmail.com>
2023-10-04 08:05:54 +0300

    Add sum calculation for 1 to n
    This commit introduces a C# program that calculates the sum of integers from 1 to 'n', where 'n' is provided by the user.
    Usage:
    - Enter a positive integer 'n'.
    - The program will calculate and display the sum of numbers from 1 to 'n' in the format "1+2+3+...+n=result".

---
+++ 04.ForLoop_Lab/02.FirstNNumbersSum/FirstNNumbersSum.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.FirstNNumbersSum
+{
+    internal class FirstNNumbersSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 1;
+            Console.Write(1);
+
+            for (int i = 2; i <= n; i++)
+            {
+                Console.Write($"+{i}");
+
+                sum += i;
+            }
+
+            Console.Write($"={sum}");
+
+        }
+    }
+}
