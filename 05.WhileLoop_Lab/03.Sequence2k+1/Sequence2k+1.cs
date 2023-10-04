aecb24d51aab1e2a96adc6ce6d8549c88833c57d
Simeon <simeonsavov21@gmail.com>
2023-10-04 07:30:27 +0300

    Add program to generate numbers following a pattern with a while loop
    This commit introduces a program that utilizes a while loop to generate a sequence of numbers based on a specific pattern. The user provides an integer maxNumber, and the program starts from 1, displaying numbers and following the pattern of doubling the current number and adding 1 until it reaches or exceeds maxNumber.

---
+++ 05.WhileLoop_Lab/03.Sequence2k+1/Sequence2k+1.cs
@@ -0,0 +1,26 @@
+﻿namespace _03.Sequence2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.Parse(Console.ReadLine());
+
+            int num = 1;
+
+            while (num <= maxNumber)
+            {
+                Console.WriteLine(num);
+                num = num * 2 + 1;
+            }
+
+            // ALTERNATIVE
+
+            //for (int i = num; i <= maxNumber; i = i * 2 + 1)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+        }
+    }
+}
