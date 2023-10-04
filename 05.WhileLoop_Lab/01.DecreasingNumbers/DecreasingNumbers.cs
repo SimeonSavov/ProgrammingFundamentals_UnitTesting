c1ab3dc8aae537fef35684b5ad04862d48eaf3f3
Simeon <simeonsavov21@gmail.com>
2023-10-04 07:27:09 +0300

    Add countdown program with a while loop
    This commit adds a C# program that counts down from a user-provided positive integer 'n' using a while loop. The program displays the countdown, starting from 'n' and decrementing by one until it reaches 1.

---
+++ 05.WhileLoop_Lab/01.DecreasingNumbers/DecreasingNumbers.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.DecreasingNumbers
+{
+    internal class DecreasingNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            while (n >= 1)
+            {
+                Console.WriteLine(n);
+                n--;
+            }
+        }
+    }
+}
