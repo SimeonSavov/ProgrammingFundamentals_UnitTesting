909c3513c6db7ee9482560914fecbbf85a27ed93
Simeon <simeonsavov21@gmail.com>
2023-10-04 07:34:58 +0300

    Add program to validate and echo numbers within a specified range
    This commit adds a program that validates user input to ensure it falls within a specified range, which in this case is between 1 and 100. The program uses a while loop to repeatedly prompt the user for input until a valid number within the range is provided.
    Usage:
    
    1. Enter an integer.
    2. The program will validate the input, ensuring it falls between 1 and 100.
    3. If the input is outside this range, it will prompt for a new input.
    4. Once a valid number within the range is provided, the program will echo and display it.

---
+++ 05.WhileLoop_Lab/05.NumbersInRange/NumbersInRange.cs
@@ -0,0 +1,20 @@
+﻿namespace _05.NumbersInRange
+{
+    internal class NumbersInRange
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            bool inRange = num >= 1 && num <= 100; // Number between 1 and 100;
+
+            while (!inRange)
+            {
+                num = int.Parse(Console.ReadLine());
+                inRange = num >= 1 && num <= 100; // Again we need to check if the number is between the range
+            }
+
+            Console.WriteLine(num);
+        }
+    }
+}
