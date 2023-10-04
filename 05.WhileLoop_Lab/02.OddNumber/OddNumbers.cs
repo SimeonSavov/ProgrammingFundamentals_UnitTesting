0858a6f050672d6a3d72b5534a5e2f4cab46277e
Simeon <simeonsavov21@gmail.com>
2023-10-04 07:29:06 +0300

    Add program to capture and display the first odd number
    This commit introduces a C# program that repeatedly reads integer inputs from the user until the first odd number is provided. Once an odd number is entered, the program echoes and displays it.

---
+++ 05.WhileLoop_Lab/02.OddNumber/OddNumbers.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.OddNumber
+{
+    internal class OddNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            while (n % 2 == 0)
+            {
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(n);
+
+
+        }
+    }
+}
