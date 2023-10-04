72bb999c6e7460dc3d2c8e0744648a07fc1d519a
Simeon <simeonsavov21@gmail.com>
2023-10-04 08:08:42 +0300

    Add number printing program
    This commit adds a C# program that prints numbers starting from 7 and increasing by 10 in each step until just before reaching 'n', where 'n' is an integer provided by the user.
    Usage:
    1. Enter a positive integer 'n'.
    2. The program will display numbers in increments of 10 from 7 until 'n' is about to be reached.

---
+++ 04.ForLoop_Lab/04.NumbersEndingIn7/NumbersEndingIn7.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.NumbersEndingIn7
+{
+    internal class NumbersEndingIn7
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 7; i < n; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
