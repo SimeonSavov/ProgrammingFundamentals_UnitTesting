0856bc139eaef838e548cefcbc90852da0f7f9d9
Simeon <simeonsavov21@gmail.com>
2023-10-04 08:03:52 +0300

    Add number range iteration using a for loop
    This commit adds a C# program that uses a `for` loop to iterate through a user-defined range of numbers. It takes a starting and ending integer as input, then displays the numbers within that range on the console.

---
+++ 04.ForLoop_Lab/01.NumbersInRange/NumbersInRange.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.NumbersInRange
+{
+    internal class NumbersInRange
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int finish = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= finish; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
