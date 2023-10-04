49e95ef33e9b6b98b10e00f26fe5ffbcce5059d1
Simeon <simeonsavov21@gmail.com>
2023-10-04 07:33:09 +0300

    Add program to calculate the sum of digits with a while loop
    This commit introduces a C# program that calculates the sum of the digits of a positive integer provided by the user. The program employs a while loop to iteratively extract and sum the individual digits of the number.
    Usage:
    
    1. Enter a positive integer.
    2. The program will calculate and display the sum of its individual digits.

---
+++ 05.WhileLoop_Lab/04.SumDigits/SumDigits.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.SumDigits
+{
+    internal class SumDigits
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10; // 1234 % 10 == 4
+                sum += digit;
+                num /= 10; // 1234 / 10 == 123 and again to the "%"
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
