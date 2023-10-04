c418e5c83daeb2833cd2e075cbbb4789d77d7b5f
Simeon <simeonsavov21@gmail.com>
2023-10-04 08:07:15 +0300

    Add number sum calculator
    This commit adds a C# program that calculates the sum of 'n' numbers entered by the user.
    Usage:
    1. Enter a positive integer 'n'.
    2. Enter 'n' numbers.
    3. The program will display the sum of these numbers.

---
+++ 04.ForLoop_Lab/03.SumNumbers/SumNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.SumNumbers
+{
+    internal class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double currentNum = double.Parse(Console.ReadLine());
+
+                sum += currentNum;
+
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
