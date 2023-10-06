08d163e357675f7b07257f692deb98390a3ce18d
Simeon <simeonsavov21@gmail.com>
2023-10-06 11:02:25 +0300

    Add code to calculate a score based on vowel occurrences
    This code calculates a score based on vowel occurrences (a, e, i, o, u) in user-input characters and prints the total score.

---
+++ 06.For&WhileLoop_Exercises/04.VowelSum/VowelSum.cs
@@ -0,0 +1,38 @@
+﻿namespace _04.VowelSum
+{
+    internal class VowelSum
+    {
+        static void Main(string[] args)
+        {
+            int countOfChar = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            for (int i = 1; i <= countOfChar; i++)
+            {
+                char currentSymbol = char.Parse(Console.ReadLine());
+
+                switch (currentSymbol)
+                {
+                    case 'a':
+                        result += 1;
+                        break;
+                    case 'e':
+                        result += 2;
+                        break;
+                    case 'i':
+                        result += 3;
+                        break;
+                    case 'o':
+                        result += 4;
+                        break;
+                    case 'u':
+                        result += 5;
+                        break;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
