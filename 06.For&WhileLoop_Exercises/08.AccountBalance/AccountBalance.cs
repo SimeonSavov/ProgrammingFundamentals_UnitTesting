4372aadfa50542db4330b86de1707071fc058702
Simeon <simeonsavov21@gmail.com>
2023-10-06 11:07:35 +0300

    Add code for account balance calculation
    This code calculates an account balance based on a sequence of user-input incomes and expenses until the user enters "End." It tracks the balance, prints increases and decreases, and finally displays the balance.

---
+++ 06.For&WhileLoop_Exercises/08.AccountBalance/AccountBalance.cs
@@ -0,0 +1,33 @@
+﻿namespace _08.AccountBalance
+{
+    internal class AccountBalance
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double balance = 0;
+
+            while (input != "End")
+            {
+                double currentNumber = double.Parse(input);
+
+                if (currentNumber > 0)
+                {
+                    balance += currentNumber;
+                    Console.WriteLine($"Increase: {currentNumber:f2}");
+                }
+                else if (currentNumber < 0) 
+                {
+
+                    balance -= Math.Abs(currentNumber);
+                    Console.WriteLine($"Decrease: {Math.Abs(currentNumber):f2}");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Balance: {balance:f2}");
+        }
+    }
+}
