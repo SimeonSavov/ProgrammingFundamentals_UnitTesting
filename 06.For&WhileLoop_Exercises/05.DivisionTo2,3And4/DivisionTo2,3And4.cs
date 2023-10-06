9531d7a6f3f5dd6561d91ba52afc2d0783030a09
Simeon <simeonsavov21@gmail.com>
2023-10-06 11:04:04 +0300

    Add code to calculate percentage of numbers divisible by 2, 3, and 4
    This code calculates and prints the percentages of numbers divisible by 2, 3, and 4 among a user-input set of numbers.

---
+++ 06.For&WhileLoop_Exercises/05.DivisionTo2,3And4/DivisionTo2,3And4.cs
@@ -0,0 +1,42 @@
+﻿namespace _05.DivisionTo2_3And4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNumbers = int.Parse(Console.ReadLine());
+
+            int count2 = 0; // Divided number by 2
+            int count3 = 0; // Divided number by 3
+            int count4 = 0; // Divided number by 4
+
+            for (int i = 1; i <= countNumbers; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+
+                if (value % 2 == 0)
+                {
+                    count2++;
+                }
+
+                if (value % 3 == 0)
+                {
+                    count3++;
+                }
+
+                if (value % 4 == 0)
+                {
+                    count4++;
+                }
+            }
+
+            double perc2 = ((double)count2 / countNumbers) * 100;
+            double perc3 = ((double)count3 / countNumbers) * 100;
+            double perc4 = ((double)count4 / countNumbers) * 100;
+
+            Console.WriteLine($"{perc2:f2}%");
+            Console.WriteLine($"{perc3:f2}%");
+            Console.WriteLine($"{perc4:f2}%");
+        }
+    }
+}
