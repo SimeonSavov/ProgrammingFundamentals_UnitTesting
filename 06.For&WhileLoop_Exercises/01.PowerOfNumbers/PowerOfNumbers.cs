49e5e7934f63ddc95129346ca5997b61e5af528b
Simeon <simeonsavov21@gmail.com>
2023-10-06 10:58:10 +0300

    Add code to calculate power with a for loop
    This code calculates the result of raising a number to a power using a for loop. It takes user inputs for the base number and the exponent.

---
+++ 06.For&WhileLoop_Exercises/01.PowerOfNumbers/PowerOfNumbers.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.PowerOfNumbers
+{
+    internal class PowerOfNumbers
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            int result = 1;
+            for (int time = 1; time <= power; time++)
+            {
+                result = result * number;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
