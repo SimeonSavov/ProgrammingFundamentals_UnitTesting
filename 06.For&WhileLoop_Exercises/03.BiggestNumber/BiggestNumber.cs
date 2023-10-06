505d75a46942b6f37668ba83618378552296f7f3
Simeon <simeonsavov21@gmail.com>
2023-10-06 11:00:50 +0300

    Add code to find the maximum number using for loop
    This code finds and prints the maximum number from a list of user-input numbers.

---
+++ 06.For&WhileLoop_Exercises/03.BiggestNumber/BiggestNumber.cs
@@ -0,0 +1,25 @@
+﻿namespace _03.BiggestNumber
+{
+    internal class BiggestNumber
+    {
+        static void Main(string[] args)
+        {
+            int countNumbers = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+
+            for (int i = 1; i <= countNumbers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+            }
+
+            Console.WriteLine(maxNumber);
+
+        }
+    }
+}
