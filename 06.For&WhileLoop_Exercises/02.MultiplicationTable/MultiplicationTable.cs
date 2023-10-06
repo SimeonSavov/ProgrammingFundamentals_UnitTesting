e643529d4b83e506da13b3065049e28b20d95b18
Simeon <simeonsavov21@gmail.com>
2023-10-06 10:59:36 +0300

    Add code for a multiplication table using a for loop
    This code generates a multiplication table for a user-input number from 1 to 10 using a for loop and displays the results.

---
+++ 06.For&WhileLoop_Exercises/02.MultiplicationTable/MultiplicationTable.cs
@@ -0,0 +1,16 @@
+﻿namespace _02.MultiplicationTable
+{
+    internal class MultiplicationTable
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} x {i} = {result}");
+            }
+        }
+    }
+}
