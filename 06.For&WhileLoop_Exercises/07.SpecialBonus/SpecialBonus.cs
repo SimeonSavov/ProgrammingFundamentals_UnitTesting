e758580f6b7508714b4a3256cd237896987e6c50
Simeon <simeonsavov21@gmail.com>
2023-10-06 11:06:24 +0300

    Add code to calculate a price increase based on a stop number
    This code calculates and prints a price increase of 20% over the previous number once a user-input stop number is encountered.

---
+++ 06.For&WhileLoop_Exercises/07.SpecialBonus/SpecialBonus.cs
@@ -0,0 +1,23 @@
+﻿namespace _07.SpecialBonus
+{
+    internal class SpecialBonus
+    {
+        static void Main(string[] args)
+        {
+            int stopNumber = int.Parse(Console.ReadLine());
+            int prevNumber = stopNumber;
+
+            while (true)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber == stopNumber)
+                {
+                    Console.WriteLine(prevNumber * 1.2);
+                    break;
+                }
+
+                prevNumber = currentNumber;
+            }
+        }
+    }
+}
