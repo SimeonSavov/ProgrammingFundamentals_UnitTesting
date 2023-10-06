e05c3077ead70aa3cc26456941db9914d29e497a
Simeon <simeonsavov21@gmail.com>
2023-10-06 11:05:20 +0300

    Add code to check if a number is "special"
    This code checks if a user-input number is "special" by verifying if it is divisible by all of its individual digits.

---
+++ 06.For&WhileLoop_Exercises/06.SpecialNumber/SpecialNumber.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _06.SpecialNumber
+{
+    internal class SpecialNumber
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int startNumber = number;
+            bool isSpecial = true;
+
+            while (number > 0)
+            {
+                int lastDiggit = number % 10; // Get last diggit of the number
+
+                if (startNumber % lastDiggit != 0)
+                {
+                    isSpecial = false;
+                    break;
+                }
+
+                number /= 10; 
+            }
+            
+
+            if (isSpecial)
+            {
+                Console.WriteLine($"{startNumber} is special"); // special
+            }
+            else
+            {
+                Console.WriteLine($"{startNumber} is not special"); // not special
+            }
+        }
+    }
+}
