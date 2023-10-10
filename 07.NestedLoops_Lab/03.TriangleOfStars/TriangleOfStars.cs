6715ba0eb8975476202e3d182c1984caa7714460
Simeon <simeonsavov21@gmail.com>
2023-10-10 13:37:59 +0300

    Add star triangle program
    This commit introduces a C# program that generates a triangle pattern made of asterisks ('*'). The program takes an integer 'size' as input and then utilizes nested for loops to create and display a triangle of stars with 'size' rows.

---
+++ 07.NestedLoops_Lab/03.TriangleOfStars/TriangleOfStars.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.TriangleOfStars
+{
+    internal class TriangleOfStars
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= size; row++)
+            {
+                for (int numberOfStar = 1; numberOfStar <= row; numberOfStar++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
