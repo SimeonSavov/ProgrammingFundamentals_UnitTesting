122c4044b5e16d9bc0cd820137c4ce7caec7c731
Simeon <simeonsavov21@gmail.com>
2023-10-10 13:36:27 +0300

    Add program to display even powers of 2 up to N
    This commit introduces a C# program that calculates and displays even powers of 2 up to a given integer 'N'. The program takes 'N' as input and then utilizes a for loop to calculate and display 2 raised to the power of even numbers from 0 up to 'N

---
+++ 07.NestedLoops_Lab/02.EvenPowersOf2/EvenPowersOf2.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.EvenPowersOf2
+{
+    internal class EvenPowersOf2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i+=2)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
