9edbec3d31e1e6d636be91d59bd71d875bdfdf2f
Simeon <simeonsavov21@gmail.com>
2023-10-10 13:45:17 +0300

    Add prime number finder program
    This commit introduces a C# program designed to find and display prime numbers within a specified range defined by the user. The program takes two integers, 'start' and 'end,' as input and utilizes an outer for loop to iterate through numbers within the given range. For each number, it employs an inner while loop with multiple conditional statements to determine if the number is prime.

---
+++ 07.NestedLoops_Lab/08.PrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,39 @@
+﻿namespace _08.PrimeNumbers
+{
+    internal class PrimeNumbers
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = start; currentNumber <= end; currentNumber++)
+            {
+                bool isPrime = true;
+                int divider = 2;
+
+                while (divider < end)
+                {
+                    if (currentNumber == divider)
+                    {
+                        divider++;
+                        continue;
+                    }
+
+                    if (currentNumber % divider == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                    divider++;
+                }
+
+                if (isPrime)
+                {
+                    Console.Write($"{currentNumber} ");
+                }
+            }
+        }
+    }
+}
