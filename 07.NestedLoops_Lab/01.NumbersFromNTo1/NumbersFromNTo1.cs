b5aff59d46777be30482dc78032a0fb3a7bae48a
Simeon <simeonsavov21@gmail.com>
2023-10-10 13:34:37 +0300

    Add countdown program
    This commit introduces a C# program that counts down from a positive integer 'N' to 1. The program takes 'N' as input, and then it utilizes a for loop to display numbers in descending order from 'N' down to 1.

---
+++ 07.NestedLoops_Lab/01.NumbersFromNTo1/NumbersFromNTo1.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.NumbersFromNTo1
+{
+    internal class NumbersFromNTo1
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
