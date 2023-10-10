01162512ab44e2f522348ee91d68b7901aeaa2ba
Simeon <simeonsavov21@gmail.com>
2023-10-10 13:40:57 +0300

    Add number pyramid program
    This commit introduces a C# program that generates a number pyramid pattern. The program takes an integer 'n' as input and utilizes nested for loops to construct a pyramid of numbers, incrementing from 1 to 'n'.

---
+++ 07.NestedLoops_Lab/05.NumberPyramid/NumberPyramid.cs
@@ -0,0 +1,32 @@
+﻿namespace _05.NumberPyramid
+{
+    internal class NumberPyramid
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            for (int rows = 1; rows < n; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    Console.Write($"{counter} ");
+                    counter++;
+
+                    if (counter > n)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine();
+
+                if (counter > n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
