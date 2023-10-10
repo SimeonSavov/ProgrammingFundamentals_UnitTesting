d3a985ddf9ee0c8fb2c4d9e7af0e3f01a901a829
Simeon <simeonsavov21@gmail.com>
2023-10-10 13:42:25 +0300

    Add travel savings tracking program
    This commit introduces a C# program designed for tracking travel savings. The program allows users to input a destination, a budget, and the amount of money they collect. It utilizes nested while loops to manage the savings process. Users can input multiple destinations and budgets, with the program continuously collecting money until the budget is reached or exceeded for each destination.

---
+++ 07.NestedLoops_Lab/06.TravelSavings/TravelSavings.cs
@@ -0,0 +1,29 @@
+﻿namespace _06.TravelSavings
+{
+    internal class TravelSavings
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while(destination != "End")
+            {
+                double collectedSum = 0;
+
+                double budget = double.Parse(Console.ReadLine());
+
+                while (collectedSum < budget)
+                {
+                    double currentSum = double.Parse(Console.ReadLine());
+                    collectedSum += currentSum;
+
+                    Console.WriteLine($"Collected: {collectedSum:f2}");
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
