609b44d91ee7410e1daed9c559b4e65b3855aebf
Simeon <simeonsavov21@gmail.com>
2023-10-10 13:43:42 +0300

    Add digit sum calculator program
    This commit introduces a C# program designed to calculate the sum of digits for a series of integers provided by the user. The program utilizes an outer while loop to repeatedly accept integer inputs until the user enters "End." For each input, it employs an inner for loop to break down the number into its individual digits and calculate their sum.

---
+++ 07.NestedLoops_Lab/07.SumOfDigitsCalculator/SumOfDigitsCalculator.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.SumOfDigitsCalculator
+{
+    internal class SumOfDigitsCalculator
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "End") 
+            {
+                int sumOfDigits = 0;
+                int currentNumber = int.Parse(input);
+
+                for (int i = currentNumber; i > 0; i /= 10)
+                {
+                    int digit = i % 10;
+                    sumOfDigits += digit;
+                }
+
+                Console.WriteLine($"Sum of digits = {sumOfDigits}");
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
