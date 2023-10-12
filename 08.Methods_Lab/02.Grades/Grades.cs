f5b9ef4660847293cf023329acb73f6895d64046
Simeon <simeonsavov21@gmail.com>
2023-10-12 13:40:33 +0300

    Add grade in words method
    In this commit, I introduced a new method called `GradeInWords` to the project. This method takes a numeric grade as input and prints the corresponding verbal description, such as "Fail," "Average," "Good," "Very good," or "Excellent." By encapsulating this logic within a method, I enhance code organization and reusability.

---
+++ 08.Methods_Lab/02.Grades/Grades.cs
@@ -0,0 +1,27 @@
+﻿static void GradeInWords(double grade)
+{
+    if (grade >= 2 && grade < 3)
+    {
+        Console.WriteLine("Fail");
+    }
+    else if (grade >= 3 && grade < 3.50)
+    {
+        Console.WriteLine("Average");
+    }
+    else if (grade >= 3.50 && grade < 4.50)
+    {
+        Console.WriteLine("Good");
+    }
+    else if (grade >= 4.50 && grade < 5.50)
+    {
+        Console.WriteLine("Very good");
+    }
+    else
+    {
+        Console.WriteLine("Excellent");
+    }
+}
+
+double grade = double.Parse(Console.ReadLine());
+
+GradeInWords(grade);
