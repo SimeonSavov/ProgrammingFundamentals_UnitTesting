7c44b0cc9e7ae802970c21890b77e96e4288b9d9
Simeon <simeonsavov21@gmail.com>
2023-10-12 13:48:43 +0300

    Add methods for calculating multiple of even and odd digits
    In this commit, I introduced a set of methods to the project for calculating the multiple of even and odd digits within an input number. The `GetMultipleOfEvenAndOdds` method orchestrates the process, utilizing two helper methods, `GetSumOfEvenDigits` and `GetSumOfOddDigits`, to compute the sums of even and odd digits separately. These methods provide a structured and reusable approach to processing numbers and determining their factors based on digit parity.

---
+++ 08.Methods_Lab/08.MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -0,0 +1,48 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+number = Math.Abs(number); // Get absolute value of the negative numbers!
+int output = GetMultipleOfEvenAndOdds(number);
+
+Console.WriteLine(output);
+
+
+static int GetMultipleOfEvenAndOdds(int number)
+{
+    int sumEven = GetSumOfEvenDigits(number);
+    int sumOdd = GetSumOfOddDigits(number);
+
+    int result = sumEven * sumOdd;
+    return result;
+}
+
+static int GetSumOfOddDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+
+        if (digit % 2 != 0) // Only the odd digits!
+        {
+            sum += digit;
+        }
+    }
+    return sum;
+}
+
+static int GetSumOfEvenDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+
+        if (digit % 2 == 0) // Only the even digits!
+        {
+            sum += digit;
+        }
+    }
+    return sum;
+}
