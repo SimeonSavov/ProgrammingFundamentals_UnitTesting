a6f7c33dd7e4d55ee419502b20bf99adb5b33c11
Simeon <simeonsavov21@gmail.com>
2023-10-12 13:42:25 +0300

    Add print line and print triangle methods
    In this commit, I introduced two new methods, `PrintLine` and `PrintTriangle`, to the project. These methods enhance the ability to create and display numerical patterns. The `PrintLine` method prints a series of numbers in a specified range, and the `PrintTriangle` method uses `PrintLine` to generate a triangle pattern based on user input. These methods promote code modularity and clarity, contributing to improved program structure.

---
+++ 08.Methods_Lab/03.PrintingTriangle/PrintingTriangle.cs
@@ -0,0 +1,26 @@
+﻿static void PrintLine(int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
+
+static void PrintTriangle(int num)
+{
+    for (int currentLine = 1; currentLine <= num; currentLine++)
+    {
+        PrintLine(1, currentLine);
+    }
+
+    for (int currentLine = num - 1; currentLine >= 1; currentLine--)
+    {
+        PrintLine(1, currentLine);
+    }
+}
+
+
+int num = int.Parse(Console.ReadLine());
+
+PrintTriangle(num);
