92e13150b1dc90e6853b34533b70ecb9fe257a47
Simeon <simeonsavov21@gmail.com>
2023-10-12 13:46:00 +0300

    Add math power method
    In this commit, I added a new method called `MathPower` to the project. This method takes a base number and a power number as input and calculates the result of raising the base number to the specified power. It simplifies mathematical power calculations and promotes code reusability. By encapsulating this operation in a method, I enhance code organization and maintainability.

---
+++ 08.Methods_Lab/06.MathPower/MathPower.cs
@@ -0,0 +1,17 @@
+﻿static int MathPower(int baseNumber, int powerNumber)
+{
+    int output = baseNumber;
+	for (int i = 0; i < powerNumber - 1; i++)
+	{
+		output *= baseNumber;
+	}
+
+	return output;
+}
+
+int baseNumber = int.Parse(Console.ReadLine());
+int powerNumber = int.Parse(Console.ReadLine());
+
+int result = MathPower(baseNumber, powerNumber);
+
+Console.WriteLine(result);
