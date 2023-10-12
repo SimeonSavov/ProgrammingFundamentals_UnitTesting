9b6f773e162a3167f7342b3661efb773f9098c00
Simeon <simeonsavov21@gmail.com>
2023-10-12 13:44:55 +0300

    Add repeat string method
    In this commit, I introduced a new method called `RepeatString` to the project. This method takes a string and a count as input and returns the string repeated a specified number of times. It simplifies the process of repeating a string and promotes code reusability. By encapsulating this operation in a method, I enhance code organization and maintainability.

---
+++ 08.Methods_Lab/05.RepeatString/RepeatString.cs
@@ -0,0 +1,18 @@
+﻿static string RepeatString(string text, int count)
+{
+    string output = "";
+	for (int i = 0; i < count; i++)
+	{
+		output += text;
+	}
+
+    return output;
+}
+
+string text = Console.ReadLine();
+int count = int.Parse(Console.ReadLine());
+
+string result = RepeatString(text, count);
+
+
+Console.WriteLine(result);
