e2feb98110c994d056043b60ed2eb277483b8614
Simeon <simeonsavov21@gmail.com>
2023-10-12 13:43:52 +0300

    Add calculate rectangle area method
    In this commit, I added a new method called `CalculateRectArea` to the project. This method takes the width and height of a rectangle as input and calculates its area, providing a structured and reusable way to perform this common mathematical operation. By encapsulating the area calculation in a method, I enhance code modularity and maintainability.

---
+++ 08.Methods_Lab/04.CalculateRectangleArea/CalculateRectangleArea.cs
@@ -0,0 +1,11 @@
+﻿static int CalculateRectArea(int width, int height)
+{
+    return width * height;
+}
+
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+
+int area = CalculateRectArea(width, height);
+
+Console.WriteLine(area);
