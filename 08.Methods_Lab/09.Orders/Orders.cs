8ab5f361cbb79d5201b03ab958b4eb9d5bcfd411
Simeon <simeonsavov21@gmail.com>
2023-10-12 13:50:11 +0300

    Add calculate price method for product pricing
    In this commit, I added a new method called `CalculatePrice` to the project. This method calculates the total price of a specified product based on the quantity provided. The method supports various products such as coffee, water, coke, and snacks, each with its own unit price. This structured approach simplifies product pricing and enhances code reusability. By encapsulating the pricing logic in a method, I improve code organization and maintainability. This update aligns with my progress in the "Methods" lesson of the course.

---
+++ 08.Methods_Lab/09.Orders/Orders.cs
@@ -0,0 +1,30 @@
+﻿string product = Console.ReadLine();
+int quantity = int.Parse(Console.ReadLine());
+
+double result = CalculatePrice(product, quantity);
+
+Console.WriteLine($"{result:f2}");
+
+static double CalculatePrice(string product, int quantity)
+{
+    double result = 0;
+
+    if (product == "coffee")
+    {
+        result = quantity * 1.5;
+    }
+    else if (product == "water")
+    {
+        result = quantity * 1;
+    }
+    else if (product == "coke")
+    {
+        result = quantity * 1.4;
+    }
+    else if (product == "snacks")
+    {
+        result = quantity * 2;
+    }
+
+    return result;
+}
