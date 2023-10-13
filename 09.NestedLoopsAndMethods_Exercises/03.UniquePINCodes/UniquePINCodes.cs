b26fd076095cea7716f15920980700702b686d12
Simeon <simeonsavov21@gmail.com>
2023-10-13 09:06:44 +0300

    Add code for finding prime digit combinations within specified ranges
    This commit adds code that calculates and displays digit combinations where the middle digit is a prime number within the specified ranges for the first and third digits.

---
+++ 09.NestedLoopsAndMethods_Exercises/03.UniquePINCodes/UniquePINCodes.cs
@@ -0,0 +1,18 @@
+﻿int max1 = int.Parse(Console.ReadLine());
+int max2 = int.Parse(Console.ReadLine());
+int max3 = int.Parse(Console.ReadLine());
+
+for (int digit1 = 2; digit1 <= max1; digit1 += 2)
+{
+	for (int digit2 = 1; digit2 <= max2; digit2++)
+	{
+		for (int digit3 = 2; digit3 <= max3; digit3 += 2)
+		{ 
+			if (digit2 == 2 || digit2 == 3 || digit2 == 5 || digit2 == 7)
+			{
+                Console.WriteLine($"{digit1}{digit2}{digit3}");
+            }
+			
+		}
+	}
+}
