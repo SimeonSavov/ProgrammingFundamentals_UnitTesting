0c4dce26b4d9dd609c91556ba6c18f5a937ab743
Simeon <simeonsavov21@gmail.com>
2023-10-13 09:09:33 +0300

    Add code for sum combinations
    This commit adds code to generate combinations of four digits that satisfy the condition where the sum of the first two digits equals 'n' and the sum of the last two digits also equals 'n'. The code uses nested loops to iterate through the possible digit values and checks for valid combinations that meet the specified criteria. These combinations are then displayed to the user.

---
+++ 09.NestedLoopsAndMethods_Exercises/05.HappyNumbers/HappyNumbers.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int digit1 = 1; digit1 <= 9; digit1++)
+{
+    for (int digit2 = 0; digit2 <= 9; digit2++)
+    {
+        for (int digit3 = 0; digit3 <= 9; digit3++)
+        {
+            for (int digit4 = 0; digit4 <= 9; digit4++)
+            {
+                if (digit1 + digit2 == n &&  digit3 + digit4 == n)
+                {
+                    Console.Write($"{digit1}{digit2}{digit3}{digit4} ");
+                }
+                
+            }
+        }
+    }
+}
