cc944671769297e0ef27580a379906404b5d284e
Simeon <simeonsavov21@gmail.com>
2023-10-13 09:07:58 +0300

    Add code for letter combinations with exclusion
    This commit introduces code for generating letter combinations within specified ranges while excluding a specific letter. It uses nested loops to iterate through the provided character range and excludes combinations containing the specified excluded letter. The total number of valid combinations is counted and displayed.

---
+++ 09.NestedLoopsAndMethods_Exercises/04.LetterCombinations/LetterCombinations.cs
@@ -0,0 +1,23 @@
+﻿char startLetter = char.Parse(Console.ReadLine());
+char endLetter = char.Parse(Console.ReadLine()); 
+char excludedLetter = char.Parse(Console.ReadLine());
+
+int count = 0;
+for (char letter1 = startLetter; letter1 <= endLetter; letter1++)
+{
+	for (char letter2 = startLetter; letter2 <= endLetter; letter2++)
+	{
+		for (char letter3 = startLetter; letter3 <= endLetter; letter3++)
+		{
+			if (letter1 != excludedLetter && letter2 != excludedLetter && letter3 != excludedLetter)
+			{
+                Console.Write($"{letter1}{letter2}{letter3} ");
+                count++;
+            }
+            
+        }
+	}
+}
+
+Console.WriteLine();
+Console.WriteLine(count);
