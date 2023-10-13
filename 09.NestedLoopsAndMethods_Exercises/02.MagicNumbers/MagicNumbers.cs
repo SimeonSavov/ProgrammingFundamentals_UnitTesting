d615fb454c24329825e3c4c7282647c88fe80835
Simeon <simeonsavov21@gmail.com>
2023-10-13 09:04:08 +0300

    Add code for finding combinations of digits that multiply to a given number
    In this solution, I've implemented a nested loop structure to find combinations of three digits that multiply together to yield the specified target number. The loops iterate through all possible combinations of digits from 0 to 9, and when a combination that meets the condition is found, it's displayed. This solution allows for exploring digit combinations that result in a given number, providing a deeper understanding of nested loop operations.

---
+++ 09.NestedLoopsAndMethods_Exercises/02.MagicNumbers/MagicNumbers.cs
@@ -0,0 +1,16 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int firstDigit = 1; firstDigit <= 9; firstDigit++)
+{
+	for (int secondDigit = 0; secondDigit <= 9; secondDigit++)
+	{
+		for (int thirdDigit = 0; thirdDigit <= 9; thirdDigit++)
+		{
+			if (firstDigit * secondDigit * thirdDigit == number)
+			{
+				Console.Write($"{firstDigit}{secondDigit}{thirdDigit} ");
+			} 
+
+        }
+	}
+}
