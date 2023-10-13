29fcd8daac6c18abb3454623450b1a6a27e95cc0
Simeon <simeonsavov21@gmail.com>
2023-10-13 09:12:08 +0300

    Add method to count vowels in a string
    This commit introduces a method, GetCountVowels, which calculates the number of vowels in a given string. The method iterates through the characters of the input string and counts occurrences of 'A', 'E', 'I', 'O', and 'U' in both uppercase and lowercase. The total count of vowels is returned as the result.

---
+++ 09.NestedLoopsAndMethods_Exercises/06.VowelsCount/VowelsCount.cs
@@ -0,0 +1,24 @@
+﻿string text = Console.ReadLine();
+
+Console.WriteLine(GetCountVowels(text));
+
+
+static int GetCountVowels(string text)
+{
+	int count = 0; // vowels count
+
+	for (int position = 0; position <= text.Length - 1; position++)
+	{
+		char currentSymbol = text[position];
+		if (currentSymbol == 'A' || currentSymbol == 'a'
+			|| currentSymbol == 'E' || currentSymbol == 'e'
+			|| currentSymbol == 'I' || currentSymbol == 'i'
+			|| currentSymbol == 'O' || currentSymbol == 'o'
+			|| currentSymbol == 'U' || currentSymbol == 'u')
+		{
+			count++;
+		}
+	}
+
+	return count;
+}
