0828df568d57c799925200b4f78f6f8aad7d0cd1
Simeon <simeonsavov21@gmail.com>
2023-10-13 09:01:53 +0300

    Add solution for nested loops exercise - even and odd numbers
    In this solution, I've tackled the exercise involving nested loops to generate even and odd numbers, along with their products. The program takes an input number and then iterates through even and odd numbers, calculating and displaying their products. This exercise enhances my understanding of nested loop structures and mathematical operations within C#.

---
+++ 09.NestedLoopsAndMethods_Exercises/01.StupidPassword/StupidPassword.cs
@@ -0,0 +1,10 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int firstNumber = 2; firstNumber <= number; firstNumber += 2) // even digit
+{
+
+	for (int secondNumber = 1; secondNumber <= number; secondNumber += 2) // odd digit
+	{
+        Console.Write($"{firstNumber}{secondNumber}{firstNumber * secondNumber} ");
+    }
+}
