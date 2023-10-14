71b44fc542fe4358cd1bee06b745735489bb0bd1
Simeon <simeonsavov21@gmail.com>
2023-10-14 08:48:50 +0300

    Add method to determine the sign of numbers
    This commit introduces the PrintSign method, which takes three integer parameters and determines the sign of their product. The method accounts for different sign combinations and provides the appropriate sign descriptor as output. This method enhances code modularity and readability.

---
+++ 09.NestedLoopsAndMethods_Exercises/08.MultiplicationSign/MultiplicationSign.cs
@@ -0,0 +1,33 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+
+PrintSign(number1, number2, number3);
+
+static void PrintSign(int n1, int n2, int n3)
+{
+    if (n1 == 0 || n2 == 0 || n3 == 0)
+    {
+        Console.WriteLine("zero");
+    }
+    else if (n1 > 0 && n2 > 0 && n3 > 0)
+    {
+        Console.WriteLine("positive");
+    }
+    else if (n1 < 0 && n2 < 0 && n3 > 0)
+    {
+        Console.WriteLine("postive");
+    }
+    else if (n1 > 0 && n2 < 0 && n3 < 0)
+    {
+        Console.WriteLine("postive");
+    }
+    else if (n1 < 0 && n2 > 0 && n3 < 0)
+    {
+        Console.WriteLine("positive");
+    }
+    else
+    {
+        Console.WriteLine("negative");
+    }
+}
