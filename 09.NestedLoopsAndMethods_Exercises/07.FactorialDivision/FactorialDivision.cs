c1da78fa6cbb03a5c2c120f705f212d7d5f5cd10
Simeon <simeonsavov21@gmail.com>
2023-10-14 08:47:22 +0300

    Add method to calculate the ratio of factorials
    This commit introduces the FactorialCalculator method, which calculates the factorial of a given number. The method iteratively computes the factorial, and it's utilized to calculate the ratio of factorials for two user-provided numbers. The result is displayed as the output of this program.

---
+++ 09.NestedLoopsAndMethods_Exercises/07.FactorialDivision/FactorialDivision.cs
@@ -0,0 +1,21 @@
+﻿static double FactorialCalculator(int number)
+{
+    double output = 1.0;
+
+    for (int factorial = 1; factorial <= number; factorial++)
+    {
+        output *= factorial;
+    }
+
+    return output;
+}
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+double factorialOne = FactorialCalculator(num1);
+double factorialTwo = FactorialCalculator(num2);
+
+double sum = factorialOne / factorialTwo;
+
+Console.WriteLine(sum);
