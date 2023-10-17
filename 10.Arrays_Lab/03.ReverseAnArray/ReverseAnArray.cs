3cbc1273f8cffcd2860a136e1e078baeeed67f8d
Simeon <simeonsavov21@gmail.com>
2023-10-17 15:36:07 +0300

    Add solution for array reversing task
     In this task, I've implemented a solution that takes an integer n as input to determine the length of an array. Then, I read a series of integers from the console to fill the array. Afterward, I reversed the order of the elements in the array and displayed the reversed array.

---
+++ 10.Arrays_Lab/03.ReverseAnArray/ReverseAnArray.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[n];
+
+for (int i = 0; i <= numbers.Length - 1; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+for (int index = numbers.Length - 1; index >= 0; index--)
+{
+    Console.Write(numbers[index] + " ");
+}
