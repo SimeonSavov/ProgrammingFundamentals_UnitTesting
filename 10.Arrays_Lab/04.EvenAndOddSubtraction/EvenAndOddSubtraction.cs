3fd54b0c6a2d63ae5f9e21b5b5f4f79fed5b043c
Simeon <simeonsavov21@gmail.com>
2023-10-17 15:37:57 +0300

    Add array even-odd difference task solution
    This commit includes the solution for finding the difference between the sum of even and odd elements within an array. The solution involves parsing input numbers, calculating the sums, and then determining and printing the difference.

---
+++ 10.Arrays_Lab/04.EvenAndOddSubtraction/EvenAndOddSubtraction.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+// Sum of even digits in the array
+int evenSum = 0;
+
+// Sum of odd digits in the array
+int oddSum = 0;
+
+foreach (int digit in numbers)
+{
+    if (digit % 2 == 0) // even digits
+    {
+        evenSum += digit;
+    }
+    else // odd digits
+    {
+        oddSum += digit;
+    }
+}
+
+// Find the diff between even and odd sum and print it
+Console.WriteLine(evenSum - oddSum);
