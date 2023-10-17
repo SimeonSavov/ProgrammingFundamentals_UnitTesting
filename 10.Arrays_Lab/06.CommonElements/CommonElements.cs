50c762f9ba8932b537fdeb0f0c7529afbe8552b3
Simeon <simeonsavov21@gmail.com>
2023-10-17 15:44:44 +0300

    Add common elements task solution
    This commit adds the solution for finding and displaying common elements between two arrays. The code reads two sets of numbers, identifies their common elements using LINQ's Intersect method, and then displays those common elements.

---
+++ 10.Arrays_Lab/06.CommonElements/CommonElements.cs
@@ -0,0 +1,6 @@
+﻿int[] firstNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] secondNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int[] commonElements = firstNumbers.Intersect(secondNumbers).ToArray(); // Return only the common elements between "firstNumbers[]" array and "secondNumbers[]" array
+
+Console.WriteLine(string.Join(" ", commonElements));
