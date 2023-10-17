6ed4461495ca0416ac556ec25f56b686c415b515
Simeon <simeonsavov21@gmail.com>
2023-10-17 15:38:50 +0300

    Add array comparison task solution
    This commit adds the solution for comparing two arrays element by element. It involves parsing two sets of input numbers, iterating through them, and checking for identical elements.

---
+++ 10.Arrays_Lab/05.EqualArrays/EqualArrays.cs
@@ -0,0 +1,19 @@
+﻿int[] firstNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int[] secondNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+bool isIdentical = true;
+
+for (int index = 0; index <= firstNumbers.Length - 1; index++)
+{
+    if (firstNumbers[index] != secondNumbers[index])
+    {
+        isIdentical = false;
+        Console.WriteLine("Arrays are not identical.");
+        break;
+    }
+}
+
+if (isIdentical)
+{
+    Console.WriteLine("Arrays are identical.");
+}
