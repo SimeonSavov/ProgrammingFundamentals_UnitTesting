dfe381dd63a239c4c4053be72556978baf63bfbd
Simeon <simeonsavov21@gmail.com>
2023-10-17 15:30:58 +0300

    Add solution for day of the week task
    In this task, I've implemented a C# program to retrieve the name of a day of the week based on the user's input number (1-7). The solution uses an array of day names and a conditional statement to ensure that the input is within a valid range. If the input is valid, it displays the corresponding day's name; otherwise, it outputs "Invalid day!" to inform the user. This task marks the beginning of our exploration into arrays and their practical applications in C# programming.

---
+++ 10.Arrays_Lab/01.DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,12 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+string[] nameOfTheDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+if (number >= 1 && number <= 7)
+{
+    Console.WriteLine(nameOfTheDays[number - 1]); 
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
