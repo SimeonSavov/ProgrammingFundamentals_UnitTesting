ef0da744c8c0a14da9ad8587db85169ac7788cc4
Simeon <simeonsavov21@gmail.com>
2023-10-17 15:33:55 +0300

    Add solution for array summing task
    In this task, I've created a C# program that calculates the sum of elements in an array. The program takes an array of integers as input, calculates the sum of its elements, and then displays the result.

---
+++ 10.Arrays_Lab/02.SumAnArray/SumAnArray.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+int sum = 0; // Sum of the elements from the array
+
+for (int index = 0; index <= numbers.Length - 1; index++)
+{
+    sum += numbers[index];
+}
+
+Console.WriteLine(sum);
