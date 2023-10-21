522e4952612f1d2ff57cc15affd37328ecbfc030
Simeon <simeonsavov21@gmail.com>
2023-10-21 21:07:57 +0300

    Add product sorting solution
    Developed a C# program that reads a list of products, sorts them in ascending order by product name, and displays the sorted list with product numbers. The program uses a list, sorting algorithm, and iteration to accomplish the task efficiently.

---
+++ 11.Lists_Lab/02.ListOfProducts/ListOfProducts.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine()); // Count of the product from the console
+
+List<string> products = new List<string>(); // products
+
+for (int index = 1; index <= n; index++)
+{
+    string product = Console.ReadLine(); // Current product
+    products.Add(product); // Add the current product to the List
+}
+
+// Full List with products
+// Ascending Order by name from A to Z
+products.Sort();
+
+int number = 1; // Current number of the product
+foreach (string product in products)
+{
+    Console.WriteLine($"{number}.{product}");
+    number++;
+}
