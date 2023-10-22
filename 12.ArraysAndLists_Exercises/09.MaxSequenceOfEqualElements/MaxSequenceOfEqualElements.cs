bd2b701b87db53c85dc64b7cf77104791f99359d
Simeon <simeonsavov21@gmail.com>
2023-10-22 10:49:15 +0300

    Add code solution for finding the longest sequence of equal elements
    This code solution is designed to find the longest sequence of equal elements in a list of integers. It iterates through the list, tracking the current length of the sequence and the maximum length found so far. If a longer sequence is encountered, the program updates the maximum length and the associated element. The final output is a list containing the element with the longest sequence repeated the corresponding number of times.

---
+++ 12.ArraysAndLists_Exercises/09.MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,32 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> output = new List<int>(); 
+
+int maxLength = 1; // Initialize the maximum length to 1
+int currentLength = 1; // Initialize the current length to 1
+int number = numbers[0]; // Initialize the current number
+
+for (int i = 1; i < numbers.Count; i++)
+{
+    if (numbers[i] == numbers[i - 1])
+    {
+        currentLength++;
+
+        if (currentLength > maxLength)
+        {
+            maxLength = currentLength;
+            number = numbers[i]; // Update the number if the current sequence is longer
+        }
+    }
+    else
+    {
+        currentLength = 1; // Reset the current length
+    }
+}
+
+for (int i = 0; i < maxLength; i++)
+{
+    output.Add(number);
+}
+
+Console.WriteLine(string.Join(" ", output));
