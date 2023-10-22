318b09ada6a6be1e7f85632807c645dbc79ba3e2
Simeon <simeonsavov21@gmail.com>
2023-10-22 10:40:57 +0300

    Add solution for summing paired elements and handling a middle element in a list
    In this solution, I tackle the task of working with a list of integers. I calculate the sum of paired elements from both ends of the list, and when the list contains an odd number of elements, I handle the middle element separately. This code demonstrates my ability to manipulate lists and efficiently handle various scenarios, such as even and odd element counts.

---
+++ 12.ArraysAndLists_Exercises/01.Gauss'Trick/Gauss'Trick.cs
@@ -0,0 +1,18 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+
+List<int> output = new List<int>(); // Empty list for the output
+
+for (int i = 0; i < numbers.Count / 2; i++)
+{
+    int sum = numbers[0 + i] + numbers[numbers.Count - 1 - i];
+    output.Add(sum);
+}
+
+if (numbers.Count % 2 != 0) // Odd count numbers in the list
+{
+    int middleDigit = numbers[numbers.Count / 2]; // Get the middle digit
+    output.Add(middleDigit);
+}
+
+Console.WriteLine(string.Join(" ", output));
