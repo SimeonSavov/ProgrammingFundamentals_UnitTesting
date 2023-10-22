02b586c9211b646517807b2ffc87e86eff19f403
Simeon <simeonsavov21@gmail.com>
2023-10-22 10:42:15 +0300

    Add solution for iterative summation of adjacent list elements
    In this code, I address the task of iteratively summing adjacent elements in a list. I start with an initial list of integers and continue merging adjacent elements until only one element remains. This solution demonstrates my skills in list manipulation, iterative algorithms, and the efficient handling of data structures.

---
+++ 12.ArraysAndLists_Exercises/02.CondenseArrayToNumber/CondenseArrayToNumber.cs
@@ -0,0 +1,20 @@
+﻿List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList(); // List from the Console
+
+while (input.Count > 1)
+{
+    List<int> output = new List<int>(); // Empty list for the output
+
+    for (int i = 0; i < input.Count - 1; i++)
+    {
+        int num1 = input[i];
+        int num2 = input[i + 1];
+
+        int sum = num1 + num2;
+        output.Add(sum);
+    }
+
+    input = output;
+    
+}
+
+Console.WriteLine(input[0]);
