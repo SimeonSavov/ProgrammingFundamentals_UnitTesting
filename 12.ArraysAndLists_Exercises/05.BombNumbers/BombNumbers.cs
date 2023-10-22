7a3f21200afffe45cd850170ea4c160d2014d0b4
Simeon <simeonsavov21@gmail.com>
2023-10-22 10:44:48 +0300

    Add solution for handling bombs in a list
    In this code, I tackle the task of dealing with bombs in a list of numbers. By identifying the bomb's number and its power, I remove a range of elements from the list centered around the bomb's location. This solution demonstrates my ability to efficiently manipulate lists and handle complex scenarios involving data removal. The final result is the sum of the remaining numbers in the list.

---
+++ 12.ArraysAndLists_Exercises/05.BombNumbers/BombNumbers.cs
@@ -0,0 +1,32 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> bomb = Console.ReadLine().Split().Select(int.Parse).ToList();
+int bombNum = bomb[0];
+int bombPow = bomb[1];
+
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (numbers[i] == bombNum)
+    {
+        int startIndex = i - bombPow;
+        int removeCount = bombPow + bombPow + 1;
+
+        if (startIndex < 0)
+        {
+            startIndex = 0;
+        }
+
+        if (startIndex + removeCount > numbers.Count)
+        {
+            removeCount = numbers.Count - startIndex;
+        }
+
+        numbers.RemoveRange(startIndex, removeCount);
+        i = -1;
+    }
+}
+
+int sum = numbers.Sum();
+
+Console.WriteLine(sum);
