2d0674fc5e23ac3dd0585bfd74b297fa2f99604a
Simeon <simeonsavov21@gmail.com>
2023-10-22 10:46:24 +0300

    Add solution for a card game between two players
    In this code, I implement a card game simulation where two players compete. Each player has their deck of cards, and the game continues until one of the players runs out of cards. The players take turns comparing their top cards, and the winner accumulates both cards into their deck. This solution showcases my ability to manage game mechanics and make decisions based on card values. The game concludes with a victory message for the winning player, along with the sum of their remaining card values

---
+++ 12.ArraysAndLists_Exercises/06.CardsGame/CardsGame.cs
@@ -0,0 +1,30 @@
+﻿List<int> firstPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> secondPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+while (firstPlayer.Count > 0 && secondPlayer.Count > 0) // Play while the two players have cards
+{
+    if (firstPlayer[0] > secondPlayer[0])
+    {
+        firstPlayer.Add(secondPlayer[0]); // First - the winner get the opponent card to his deck
+        firstPlayer.Add(firstPlayer[0]); // Second - the winner get his own card to the deck
+    }
+    else if (firstPlayer[0] < secondPlayer[0])
+    {
+        secondPlayer.Add(firstPlayer[0]); // First - the winner get the opponent card to his deck
+        secondPlayer.Add(secondPlayer[0]); // Second - the winner get his own card to the deck
+    }
+
+    firstPlayer.RemoveAt(0); // Remove those cards
+    secondPlayer.RemoveAt(0);
+}
+
+if (firstPlayer.Count > 0) // Means the first player have cards in his deck
+{
+    int sum = firstPlayer.Sum();
+    Console.WriteLine($"First player wins! Sum: {sum}");
+}
+else if (secondPlayer.Count > 0) // Second player have cards in his deck
+{
+    int sum = secondPlayer.Sum();
+    Console.WriteLine($"Second player wins! Sum: {sum}");
+}
