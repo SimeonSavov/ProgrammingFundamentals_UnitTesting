c4537a902e64c66f9fa795d7c40b4dffcfa77b21
Simeon <simeonsavov21@gmail.com>
2023-10-22 10:49:56 +0300

    Add code solution for finding top integers in a list
    This code solution is aimed at identifying and extracting the "top integers" from a given list of integers. An integer is considered a "top integer" if it is greater than all the integers to its right in the list. The code iterates through the list and, for each integer, checks if it is greater than or equal to all the integers to its right. If an integer meets this criterion, it is identified as a "top integer" and added to a separate list. The final output is a list containing all the "top integers" found in the original list.

---
+++ 12.ArraysAndLists_Exercises/10.TopIntegers/TopIntegers.cs
@@ -0,0 +1,24 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> topIntegers = new List<int>();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    bool isTopInteger = true;
+
+	for (int j = i + 1; j < numbers.Count; j++)
+	{
+		if (numbers[i] <= numbers[j])
+		{
+			isTopInteger = false;
+			break;
+		}
+	}
+
+	if (isTopInteger)
+	{
+		topIntegers.Add(numbers[i]);
+	}
+}
+
+Console.WriteLine(string.Join(" ", topIntegers));
