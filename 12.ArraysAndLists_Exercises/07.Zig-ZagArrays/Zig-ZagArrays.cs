994c6476203151127762217d4742caff44b3eb98
Simeon <simeonsavov21@gmail.com>
2023-10-22 10:47:42 +0300

    Add code solution for splitting and rearranging numbers
    In this code, I work with a sequence of numbers and split them into two separate lists, alternating the arrangement based on the row index. The input consists of pairs of numbers, and for each pair, I determine whether it should be added to the first or second list based on whether the row is even or odd. This solution demonstrates my proficiency in handling data transformations and list operations, resulting in the correct organization of numbers into two distinct lists. The output displays both lists as specified in the problem description.

---
+++ 12.ArraysAndLists_Exercises/07.Zig-ZagArrays/Zig-ZagArrays.cs
@@ -0,0 +1,26 @@
+﻿int numCount = int.Parse(Console.ReadLine());
+
+List<int> firstList = new List<int>();
+List<int> secondList = new List<int>();
+
+for (int i = 0; i < numCount; i++)
+{
+    List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList(); // Read two integers from the console and add it to the empty list;
+
+    int num1 = numbers[0]; // Get the first digit from the list with index "0";
+    int num2 = numbers[1]; // Get the second digit from the list with index "1";
+
+    if (i % 2 == 0) // Even rows
+    {
+        firstList.Add(num1);
+        secondList.Add(num2);
+    }
+    else // Odd rows
+    {
+        firstList.Add(num2);
+        secondList.Add(num1);
+    }
+}
+
+Console.WriteLine(string.Join(" ", firstList));
+Console.WriteLine(string.Join(" ", secondList));
