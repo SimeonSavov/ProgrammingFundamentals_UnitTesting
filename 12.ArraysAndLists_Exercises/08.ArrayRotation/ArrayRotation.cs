c17b8c6ad57fe5497389f4838b4e1adfe467987a
Simeon <simeonsavov21@gmail.com>
2023-10-22 10:48:31 +0300

    Add code solution for rotating a list of integers
    This code solution allows you to rotate a list of integers a specified number of times. The program takes the first number in the list and moves it to the end of the list, repeating this process according to the specified number of rotations. The rotated list is then displayed as the output.

---
+++ 12.ArraysAndLists_Exercises/08.ArrayRotation/ArrayRotation.cs
@@ -0,0 +1,12 @@
+﻿List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+int rotations = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotations; i++)
+{
+    int currentNumber = input[0];
+    input.RemoveAt(0); // Remove the first digit in index "0"
+    input.Add(currentNumber); // Add the digit in last position of the list
+}
+
+Console.WriteLine(string.Join(" ", input));
