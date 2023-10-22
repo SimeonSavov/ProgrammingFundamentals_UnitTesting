d198a70226f21ea913eadb294b96aafafaafa890
Simeon <simeonsavov21@gmail.com>
2023-10-22 10:43:10 +0300

    Add solution for merging two lists element-wise
    In this code, I tackle the task of merging two lists element-wise, combining corresponding elements from two separate lists into a new list. To ensure a seamless merge, I handle cases where one of the lists may be longer than the other. This solution showcases my proficiency in list operations, conditional statements, and efficient data manipulation.

---
+++ 12.ArraysAndLists_Exercises/03.MergingLists/MergingLists.cs
@@ -0,0 +1,22 @@
+﻿List<int> numbers1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> numbers2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> output = new List<int>();
+
+int longerCount = Math.Max(numbers1.Count , numbers2.Count); // Get the longer list
+
+for (int i = 0; i < longerCount; i++)
+{
+    if (i < numbers1.Count)
+    {
+        output.Add(numbers1[i]);
+    }
+    
+    if (i < numbers2.Count)
+    {
+        output.Add(numbers2[i]);
+    }
+}
+
+
+Console.WriteLine(string.Join(" ", output));
