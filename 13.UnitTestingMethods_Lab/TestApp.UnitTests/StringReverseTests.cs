ad8ffb5ff3012a126d7c388c961446deb07e5c9b
Simeon <simeonsavov21@gmail.com>
2023-10-24 16:42:19 +0300

    Add unit tests for the StringReverse class
    This commit includes unit tests for the `StringReverse` class, which verifies the correctness of the string reversal functionality. The tests cover various scenarios:
    
    1. **Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString:** This test checks if the `Reverse` method correctly handles an empty input string and returns an empty string.
    
    2.**Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter:** In this test, the `Reverse` method is evaluated with a single-character input, ensuring that it returns the same character.
    
    3.**Test_Reverse_WhenGivenNormalString_ReturnsReversedString:** This test examines the reversal of a typical string and verifies that the method produces the correct reversed string with the same length as the expected result.
    
    These tests provide comprehensive coverage for the `StringReverse` class, ensuring the accurate reversal of strings under various conditions.

---
+++ 13.UnitTestingMethods_Lab/TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = ""; // string.Empty --> is the same as a "";
+
+        // Act
+        string actual = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("", actual);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        // Arrange
+        string input = "X";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+
+        Assert.AreEqual("X", result);
+        Assert.AreEqual(1, result.Length);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        // Arrange
+        string input = "XYZ";
+
+        // Act
+        string actual = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("ZYX", actual);
+        Assert.AreEqual(input.Length, actual.Length); // It should be same length as the expected
+    }
+}
