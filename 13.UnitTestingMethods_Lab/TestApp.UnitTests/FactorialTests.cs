a22ecae140cd5dfdcd595b80eb46799d078a559b
Simeon <simeonsavov21@gmail.com>
2023-10-24 16:30:54 +0300

    Add unit tests for the Factorial class
    This commit includes a set of unit tests for the `Factorial` class in the "TestApp" namespace. These tests cover various scenarios, including calculating the factorial of zero, calculating the factorial of a positive number, and handling cases where a negative number is provided as input. The unit tests use the NUnit testing framework and provide comprehensive test coverage for the `CalculateFactorial` method, ensuring that it behaves as expected and handles exceptions correctly.

---
+++ 13.UnitTestingMethods_Lab/TestApp.UnitTests/FactorialTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.UnitTests;
+
+public class FactorialTests
+{
+    [Test]
+    public void CalculateFactorial_InputZero_ReturnsOne()
+    {
+        // Arrange
+        int input = 0;
+
+        // Act
+        int actual = Factorial.CalculateFactorial(input);
+
+        // Assert
+        Assert.AreEqual(1, actual);
+    }
+
+    [Test]
+    public void CalculateFactorial_InputPositiveNumber_ReturnsCorrectFactorial()
+    {
+        // Arrange
+        int input = 3;
+
+        // Act
+        int actual = Factorial.CalculateFactorial(input);
+
+        // Assert
+        Assert.AreEqual(6, actual);
+    }
+
+    [Test]
+    public void CalculateFactorial_InputNegativeNumber_ThrowsException()
+    {
+        // Arrange
+        int input = -1;
+
+        // Act + Assert
+        //int actual = Factorial.CalculateFactorial(input);
+        Assert.Throws<ArgumentOutOfRangeException>(() => { Factorial.CalculateFactorial(input); });
+    }
+}
