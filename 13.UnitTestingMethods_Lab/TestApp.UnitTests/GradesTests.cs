ae7141d0468e4cd3de9aff90cb0e71247efe7652
Simeon <simeonsavov21@gmail.com>
2023-10-24 16:35:04 +0300

    Add unit tests for the Grades class
    In this commit, a series of unit tests for the `Grades` class are included within the "GradesTests" class in the "TestApp.UnitTests" namespace. These tests are designed to verify the correctness of the `GradeAsWords` method, which converts numeric grades to their corresponding word-based equivalents. The test cases cover a range of input grades and expected output strings, ensuring that the method correctly handles grade boundaries, edge cases, and invalid inputs. These unit tests provide a comprehensive validation of the `Grades` class's functionality, enhancing its reliability and accuracy in grade conversion.

---
+++ 13.UnitTestingMethods_Lab/TestApp.UnitTests/GradesTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class GradesTests
+{
+    [TestCase(2.00, "Fail")]
+    [TestCase(3.00, "Average")]
+    [TestCase(3.50, "Good")]
+    [TestCase(4.00, "Very Good")]
+    [TestCase(4.50, "Excellent")]
+    public void Test_GradeAsWords_ReturnsCorrectString(double grade, string expected)
+    {
+        // Arrange
+
+
+        // Act
+        string actual = Grades.GradeAsWords(grade);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestCase(1.99, "Invalid!")]
+    [TestCase(0, "Invalid!")]
+    [TestCase(-1, "Invalid!")]
+    public void Test_GradeAsWords_ReturnsCorrectString_EdgeCases(double grade, string expected)
+    {
+        // Arrange
+
+        // Act
+        string actual = Grades.GradeAsWords(grade);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
