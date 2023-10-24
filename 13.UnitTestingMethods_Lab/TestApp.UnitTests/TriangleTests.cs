227e2eb68ecbcd25bb46fd66d6341e5f225e2298
Simeon <simeonsavov21@gmail.com>
2023-10-24 16:46:36 +0300

    Add unit tests for the Triangle class
    This commit includes the unit tests for the `Triangle` class. These tests ensure that the `PrintTriangle` method functions correctly by comparing its output with the expected patterns for various input sizes.
    
    The tests cover scenarios with different sizes, including size 0, 3, and 5, verifying that the generated triangle patterns match the expected patterns. These unit tests are crucial for confirming the accuracy and reliability of the `Triangle` class in generating triangle patterns.
    
    These tests are an essential part of ensuring that the `Triangle` class produces the desired output for different input sizes, and they will serve as a valuable reference for future development and maintenance of this class.

---
+++ 13.UnitTestingMethods_Lab/TestApp.UnitTests/TriangleTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.UnitTests;
+
+public class TriangleTests
+{
+    [Test]
+    public void Test_Triangle_OutputMatchesExpected_Size0()
+    {
+        // Arrange
+        int input = 0;
+
+        // Act
+        string actual = Triangle.PrintTriangle(input);
+
+        // Assert
+        Assert.AreEqual("", actual);
+
+    }
+
+    [Test]
+    public void Test_Triangle_OutputMatchesExpected_Size3()
+    {
+        // Arrange
+        int input = 3;
+        string expectedOutput = "1" + Environment.NewLine +
+            "1 2" + Environment.NewLine +
+            "1 2 3" + Environment.NewLine +
+            "1 2" + Environment.NewLine +
+            "1";
+        // Act
+        string actual = Triangle.PrintTriangle(input);
+
+        // Assert
+        Assert.AreEqual(expectedOutput, actual);
+    }
+
+    [Test]
+    public void Test_Triangle_OutputMatchesExpected_Size5()
+    {
+        // Arrange
+        int input = 5;
+        string expectedOutput = "1" + Environment.NewLine +
+            "1 2" + Environment.NewLine +
+            "1 2 3" + Environment.NewLine +
+            "1 2 3 4" + Environment.NewLine +
+            "1 2 3 4 5" + Environment.NewLine +
+            "1 2 3 4" + Environment.NewLine +
+            "1 2 3" + Environment.NewLine +
+            "1 2" + Environment.NewLine +
+            "1";
+        // Act
+        string actual = Triangle.PrintTriangle(input);
+
+        // Assert
+        Assert.AreEqual(expectedOutput, actual);
+    }
+}
