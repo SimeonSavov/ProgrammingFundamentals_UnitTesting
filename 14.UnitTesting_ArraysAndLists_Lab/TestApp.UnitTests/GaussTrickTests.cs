76cd3c097b8ef9fb39c222c287f3fdbba085153f
Simeon <simeonsavov21@gmail.com>
2023-10-25 07:45:41 +0300

    Add unit tests for GaussTrick class
    Added a set of unit tests for the "GaussTrick" class, including tests to verify the behavior of the "SumPairs" method. The tests cover scenarios such as empty input lists, lists with single elements, lists with even and odd counts of elements, and larger lists. These tests ensure the correctness and robustness of the "GaussTrick" class for summing pairs of list elements efficiently.

---
+++ 14.UnitTesting_ArraysAndLists_Lab/TestApp.UnitTests/GaussTrickTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class GaussTrickTests
+{
+    [Test]
+    public void Test_SumPairs_InputIsEmptyList_ShouldReturnEmptyList()
+    {
+        // Arrange
+        List<int> emptyList = new();
+
+        // Act
+        List<int> result = GaussTrick.SumPairs(emptyList);
+
+        // Assert
+        CollectionAssert.AreEqual(emptyList, result);
+    }
+
+
+    [Test]
+    public void Test_SumPairs_InputHasSingleElement_ShouldReturnSameElement()
+    {
+        // Arrange
+        List<int> list = new() { 42 };
+
+        // Act
+        List<int> result = GaussTrick.SumPairs(list);
+
+        // Assert
+        CollectionAssert.AreEqual(list, result);
+    }
+
+
+    [Test]
+    public void Test_SumPairs_InputHasEvenCountElements_ShouldReturnSumPairs()
+    {
+        // Arrange
+        List<int> list = new() { 1, 2, 3, 4 };
+
+        // Act
+        List<int> result = GaussTrick.SumPairs(list);
+
+        // Assert
+        List<int> assertResult = new List<int>() { 5, 5};
+        CollectionAssert.AreEqual(result, assertResult);
+    }
+
+    [Test]
+    public void Test_SumPairs_InputHasOddCountElements_ShouldReturnWithMiddleElement()
+    {
+        // Arrange
+        List<int> list = new() { 1, 2, 3, 4, 5};
+
+        // Act
+        List<int> result = GaussTrick.SumPairs(list);
+
+        // Assert
+        List<int> assertResult = new List<int>() { 6, 6, 3 };
+        CollectionAssert.AreEqual(result, assertResult);
+    }
+
+    [Test]
+    public void Test_SumPairs_InputHasLargeEvenCountElements_ShouldReturnSumPairs()
+    {
+        // Arrange
+        List<int> list = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Act
+        List<int> result = GaussTrick.SumPairs(list);
+
+        // Assert
+        List<int> assertResult = new List<int>() { 11, 11, 11, 11, 11 };
+        CollectionAssert.AreEqual(result, assertResult);
+    }
+
+    [Test]
+    public void Test_SumPairs_InputHasLargeOddCountElements_ShouldReturnWithMiddleElement()
+    {
+        // Arrange
+        List<int> list = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+        // Act
+        List<int> result = GaussTrick.SumPairs(list);
+
+        // Assert
+        List<int> assertResult = new List<int>() { 12, 12, 12, 12, 12, 6 };
+        CollectionAssert.AreEqual(result, assertResult);
+    }
+}
