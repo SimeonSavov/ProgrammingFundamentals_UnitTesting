cc392a3a04d6da5de908cbd773320bac89f76800
Simeon <simeonsavov21@gmail.com>
2023-10-25 07:51:41 +0300

    Add unit tests for Reverse class
    Included a set of unit tests for the "Reverse" class. These tests validate the behavior of the "ReverseArray" method in various scenarios:
    
    1.Test_ReverseArray_InputIsEmpty_ShouldReturnEmptyString: Verifies that the method returns an empty string when the input array is empty.
    
    2.Test_ReverseArray_InputHasOneElement_ShouldReturnTheSameElement: Checks if the method correctly returns the single element in the array as a string.
    
    3.Test_ReverseArray_InputHasMultipleElements_ShouldReturnReversedString: Validates that the method accurately reverses the order of elements in the input array and returns them as a space-separated string.

---
+++ 14.UnitTesting_ArraysAndLists_Lab/TestApp.UnitTests/ReverseTests.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class ReverseTests
+{
+    [Test]
+    public void Test_ReverseArray_InputIsEmpty_ShouldReturnEmptyString()
+    {
+        // Arrange
+        int[] emptyArray = Array.Empty<int>();
+
+        // Act
+        string result = Reverse.ReverseArray(emptyArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(string.Empty));
+    }
+
+    
+    [Test]
+    public void Test_ReverseArray_InputHasOneElement_ShouldReturnTheSameElement()
+    {
+        // Arrange
+        int[] oneElementArray =  new int[1] { 42 };
+
+        // Act
+        string result = Reverse.ReverseArray(oneElementArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("42"));
+    }
+
+    [Test]
+    public void Test_ReverseArray_InputHasMultipleElements_ShouldReturnReversedString()
+    {
+        // Arrange
+        int[] array = new int[5] { 1, 2, 3, 4, 5 };
+
+        // Act
+        string result = Reverse.ReverseArray(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("5 4 3 2 1"));
+    }
+}
