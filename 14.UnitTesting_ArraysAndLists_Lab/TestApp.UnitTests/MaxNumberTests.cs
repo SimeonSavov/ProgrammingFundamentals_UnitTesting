9cdac479c1932b2573c370816633a8d26b0ae760
Simeon <simeonsavov21@gmail.com>
2023-10-25 07:47:59 +0300

    Add unit tests for MaxNumber class
    Implemented a set of unit tests for the "MaxNumber" class, covering various scenarios for finding the maximum number in a list of integers. These tests ensure that the "FindMax" method correctly identifies the maximum value, handles null and empty input lists by throwing exceptions, and successfully returns the maximum in lists with positive, negative, mixed, or duplicate maximum values. These tests provide comprehensive coverage for the MaxNumber class and validate its functionality.

---
+++ 14.UnitTesting_ArraysAndLists_Lab/TestApp.UnitTests/MaxNumberTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class MaxNumberTests
+{
+    [Test]
+    public void Test_FindMax_InputIsNull_ShouldThrowArgumentException()
+    {
+        // Arrange
+        List<int>? nullList = null;
+
+        // Act & Assert
+        Assert.That(() => MaxNumber.FindMax(nullList), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_FindMax_InputIsEmptyList_ShouldThrowArgumentException()
+    {
+        // Arrange
+        List<int> emptyList = new();
+
+        // Act & Assert
+        Assert.That(() => MaxNumber.FindMax(emptyList), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_FindMax_InputHasOneElement_ShouldReturnTheElement()
+    {
+        // Arrange
+        List<int> oneElementList = new() { 42 };
+
+        // Act
+        int result = MaxNumber.FindMax(oneElementList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(42));
+    }
+
+    [Test]
+    public void Test_FindMax_InputHasPositiveIntegers_ShouldReturnMaximum()
+    {
+        // Arrange
+        List<int> positiveList = new() { 1, 2, 3, 4, 5, 42 };
+
+        // Act
+        int result = MaxNumber.FindMax(positiveList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(42));
+    }
+
+    [Test]
+    public void Test_FindMax_InputHasNegativeIntegers_ShouldReturnMaximum()
+    {
+        // Arrange
+        List<int> negativeList = new() { -2, - 100, - 1, -42 };
+
+        // Act
+        int result = MaxNumber.FindMax(negativeList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void Test_FindMax_InputHasMixedIntegers_ShouldReturnMaximum()
+    {
+        // Arrange
+        List<int> mixedList = new() { -2, 100, -1, 42 };
+
+        // Act
+        int result = MaxNumber.FindMax(mixedList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(100));
+    }
+
+    [Test]
+    public void Test_FindMax_InputHasDuplicateMaxValue_ShouldReturnMaximum()
+    {
+        // Arrange
+        List<int> duplicateMaxList = new() {100, -2, 100, -1, 42, 100 };
+
+        // Act
+        int result = MaxNumber.FindMax(duplicateMaxList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(100));
+    }
+}
