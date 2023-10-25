cba87e4e13b84fcffeb0a055dbf458d365082921
Simeon <simeonsavov21@gmail.com>
2023-10-25 07:41:21 +0300

    Add unit tests for Average class
    Added a set of unit tests for the "Average" class, covering various scenarios. These tests ensure the correctness of the "CalculateAverage" method when provided with an empty array, an array with a single element, arrays with positive integers, arrays with negative integers, and arrays with mixed integers. The tests validate that the average is calculated accurately in each case. This comprehensive test suite guarantees the reliability of the "Average" class for average calculation.

---
+++ 14.UnitTesting_ArraysAndLists_Lab/TestApp.UnitTests/AverageTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class AverageTests
+{
+    [Test]
+    public void Test_CalculateAverage_InputIsEmptyArray_ShouldThrowArgumentException()
+    {
+        // Arrange
+        int[] emptyArray = Array.Empty<int>();
+
+        // Act & Assert
+        Assert.That(() => Average.CalculateAverage(emptyArray), Throws.ArgumentException);
+    }
+
+
+    [Test]
+    public void Test_CalculateAverage_InputHasOneElement_ShouldReturnSameElement()
+    {
+        // Arrange
+        int[] array = { 42 };
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(42));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasPositiveIntegers_ShouldReturnCorrectAverage()
+    {
+        // Arrange
+        int[] array = { 1, 2, 3 };
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasNegativeIntegers_ShouldReturnCorrectAverage()
+    {
+        // Arrange
+        int[] array = { -10, -20 };
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(-15));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasMixedIntegers_ShouldReturnCorrectAverage()
+    {
+        // Arrange
+        int[] array = { 1, 2, 3, -1, -2 };
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(0.6));
+    }
+}
