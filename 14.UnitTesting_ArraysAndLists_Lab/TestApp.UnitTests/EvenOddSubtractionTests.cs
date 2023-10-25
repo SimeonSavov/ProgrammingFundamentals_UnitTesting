57edd44fb8cdcfc5e67ac08b37fbfff791224dc2
Simeon <simeonsavov21@gmail.com>
2023-10-25 07:43:29 +0300

    Add unit tests for EvenOddSubtraction class
    Added a set of unit tests for the "EvenOddSubtraction" class to ensure the correctness of the "FindDifference" method. These tests cover various scenarios, including an empty input, arrays with only even or odd numbers, and arrays with mixed numbers. The unit tests provide comprehensive test coverage for this utility, ensuring it behaves as expected in different cases.

---
+++ 14.UnitTesting_ArraysAndLists_Lab/TestApp.UnitTests/EvenOddSubtractionTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class EvenOddSubtractionTests
+{
+    [Test]
+    public void Test_FindDifference_InputIsEmpty_ShouldReturnZero()
+    {
+        // Arrange
+        int[] emptyArray = Array.Empty<int>();
+
+        // Act
+        int result = EvenOddSubtraction.FindDifference(emptyArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Test_FindDifference_InputHasOnlyEvenNumbers_ShouldReturnEvenSum()
+    {
+        // Arrange
+        int[] evenArray = new int[2] { 10, 10 };
+
+        // Act
+        int result = EvenOddSubtraction.FindDifference(evenArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(20));
+    }
+
+    [Test]
+    public void Test_FindDifference_InputHasOnlyOddNumbers_ShouldReturnNegativeOddSum()
+    {
+        // Arrange
+        int[] oddArray = new int[2] { 9, 11 };
+
+        // Act
+        int result = EvenOddSubtraction.FindDifference(oddArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(20));
+    }
+
+    [Test]
+    public void Test_FindDifference_InputHasMixedNumbers_ShouldReturnDifference()
+    {
+        // Arrange
+        int[] mixedArray = new int[3] { 9, 11, 13 };
+
+        // Act
+        int result = EvenOddSubtraction.FindDifference(mixedArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(33));
+    }
+}
