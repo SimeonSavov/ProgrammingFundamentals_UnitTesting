2ff00e3f71e9f45544ae7b50e6f46cf1a6a113d2
Simeon <simeonsavov21@gmail.com>
2023-10-28 14:08:11 +0300

    Add unit tests for Matrix class
    In this commit, I have added a set of unit tests for the "Matrix" class. These tests validate the correctness and robustness of the MatrixAddition method, which performs matrix addition. The tests cover various scenarios, including:
    
    1.Addition of two valid matrices with the same dimensions, ensuring the expected result is achieved.
    2.Handling empty matrices, which should result in an empty matrix as output.
    3.Checking for different matrix dimensions, where the method should throw an ArgumentException as expected.
    4.Validating matrix addition with negative numbers, ensuring the method handles them correctly.
    5.Verifying that adding a zero matrix to another matrix results in the original matrix.
    
    These tests provide comprehensive coverage of the Matrix class's functionality and will ensure that it performs matrix addition accurately.

---
+++ 15.UnitTesting_Methods_ArraysLists_Exercises/TestApp.UnitTests/MatrixTests.cs
@@ -0,0 +1,111 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class MatrixTests
+{
+    [Test]
+    public void Test_MatrixAddition_ValidInput_ReturnsCorrectResult()
+    {
+        // Arrange
+        List<List<int>> matrixA = new() { new() { 1, 2 }, new() { 3, 4 } };
+        List<List<int>> matrixB = new() { new() { 5, 6 }, new() { 7, 8 } };
+        List<List<int>> expected = new() { new() { 6, 8 }, new() { 10, 12 } };
+
+        // Act
+        List<List<int>> actual = Matrix.MatrixAddition(matrixA, matrixB);
+
+        // Assert
+        CollectionAssert.AreEqual(actual, expected);
+    }
+
+    [Test]
+    public void Test_MatrixAddition_EmptyMatrices_ReturnsEmptyMatrix()
+    {
+        // Arrange
+        List<List<int>> matrixA = new();
+        List<List<int>> matrixB = new();
+        List<List<int>> expected = new();
+
+        // Act
+        List<List<int>> actual = Matrix.MatrixAddition(matrixA, matrixB);
+
+        // Assert
+        CollectionAssert.AreEqual(actual, expected);
+    }
+
+    [Test]
+    public void Test_MatrixAddition_DifferentDimensions_ThrowsArgumentException()
+    {
+        // Arrange
+        List<List<int>> matrixA = new() { new() { 1, 2 }, new() { 3, 4 } };
+        List<List<int>> matrixB = new() { new() { 1, 2, 3 }, new() { 3, 4, } };
+
+        // Act + Assert
+        Assert.That(() => Matrix.MatrixAddition(matrixA, matrixB), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_MatrixAddition_DifferentDimensions_ThrowsArgumentException2()
+    {
+        // Arrange
+        List<List<int>> matrixA = new() { new() { 1, 2 }, new() { 3, 4 } };
+        List<List<int>> matrixB = new();
+
+        // Act + Assert
+        Assert.That(() => Matrix.MatrixAddition(matrixA, matrixB), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_MatrixAddition_DifferentDimensions_ThrowsArgumentException3()
+    {
+        // Arrange
+        List<List<int>> matrixA = new() { new() { 1, 2 }, new() { 3, 4 } };
+        List<List<int>> matrixB = new() { new() { 1, 2 }, new() { 3, 4, 5 } };
+
+        // Act + Assert
+        Assert.That(() => Matrix.MatrixAddition(matrixA, matrixB), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_MatrixAddition_DifferentDimensions_ThrowsArgumentException4()
+    {
+        // Arrange
+        List<List<int>> matrixA = new() { new() { 1, 2 }, new() { 3, 4 } };
+        List<List<int>> matrixB = new() { new() { 1, 2, 3 }, new() { 3, 4 }, new() { 5, 6 } };
+
+        // Act + Assert
+        Assert.That(() => Matrix.MatrixAddition(matrixA, matrixB), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_MatrixAddition_NegativeNumbers_ReturnsCorrectResult()
+    {
+        // Arrange
+        List<List<int>> matrixA = new() { new() { -1, 2 }, new() { 3, -4 } };
+        List<List<int>> matrixB = new() { new() { 5, -6 }, new() { -7, 8 } };
+        List<List<int>> expected = new() { new() { 4, -4 }, new() { -4, 4 } };
+
+        // Act
+        List<List<int>> actual = Matrix.MatrixAddition(matrixA, matrixB);
+
+        // Assert
+        CollectionAssert.AreEqual(actual, expected);
+    }
+
+    [Test]
+    public void Test_MatrixAddition_ZeroMatrix_ReturnsOriginalMatrix()
+    {
+        // Arrange
+        List<List<int>> matrixA = new() { new() { -1, 2 }, new() { 3, -4 } };
+        List<List<int>> matrix0 = new() { new() { 0, 0 }, new() { 0, 0 } };
+
+        // Act
+        List<List<int>> result = Matrix.MatrixAddition(matrixA, matrix0);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(matrixA));
+    }
+}
