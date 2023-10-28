5292c0e49c0b424b0a80eff3271cb73d8c53ab14
Simeon <simeonsavov21@gmail.com>
2023-10-28 13:55:56 +0300

    Add unit tests for Email class
    In this commit, I've added a set of unit tests to validate the functionality of the Email class, particularly its IsValidEmail method. The tests cover various scenarios to ensure that email validation is handled correctly.
    
    1.Test_IsValidEmail_ValidEmail:
    In this test, we validate a valid email address ("test@example.com").
    The IsValidEmail method should return true for a valid email, which indicates that the email format is correct.
    
    2.Test_IsValidEmail_InvalidEmail:
    This test checks an invalid email address ("test.example.com").
    The IsValidEmail method should return false for an invalid email, indicating that the email format is incorrect.
    
    3.Test_IsValidEmail_WhiteSpaceEmail:
    In this test, an email address consisting only of white spaces is used.
    The IsValidEmail method should return false as white spaces are not a valid email format.
    
    4.Test_IsValidEmail_NullInput:
    This test validates the behavior of the IsValidEmail method when a null email is provided.
    The method should return false as a null input is not a valid email address.

---
+++ 15.UnitTesting_Methods_ArraysLists_Exercises/TestApp.UnitTests/EmailTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Net.Mail;
+
+namespace TestApp.UnitTests;
+
+public class EmailTests
+{
+    [Test]
+    public void Test_IsValidEmail_ValidEmail()
+    {
+        // Arrange
+        string email = "test@example.com";
+
+        // Act
+        bool result = Email.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void Test_IsValidEmail_InvalidEmail()
+    {
+        // Arrange
+        string email = "test.example.com";
+
+        // Act
+        bool result = Email.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void Test_IsValidEmail_WhiteSpaceEmail()
+    {
+        // Arrange
+        string email = "     ";
+
+        // Act
+        bool result = Email.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void Test_IsValidEmail_NullInput()
+    {
+        // Arrange
+        string email = null;
+
+        // Act
+        bool result = Email.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
