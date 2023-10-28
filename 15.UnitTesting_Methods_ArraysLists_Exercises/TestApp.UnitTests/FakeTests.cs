e6e1fd5b6231df4f8ae962225d6d57559b782b8c
Simeon <simeonsavov21@gmail.com>
2023-10-28 13:58:52 +0300

    Add unit tests for Fake class
    In this commit, I've added a set of unit tests for the "Fake" class's RemoveStringNumbers method. These tests cover various scenarios to ensure the functionality and correctness of the method. The tests include cases for null input, removing digits from a character array, handling arrays with only digits, arrays with no digits, and empty arrays. The unit tests are now available for verifying the behavior of the "Fake" class as part of the fifteenth section of the course, focusing on unit testing methods for arrays and lists.

---
+++ 15.UnitTesting_Methods_ArraysLists_Exercises/TestApp.UnitTests/FakeTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class FakeTests
+{
+    [Test]
+    public void Test_RemoveStringNumbers_NullInput_ThrowsException()
+    {
+        // Arrange
+        char[]? input = null;
+
+        // Act + Assert
+        Assert.That(() => Fake.RemoveStringNumbers(input), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_RemovesDigitsFromCharArray()
+    {
+        // Arrange
+        char[] arr = new char[] {'0', '1', 'a', 'b' };
+
+        // Act
+        char[] result = Fake.RemoveStringNumbers(arr);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new char[] { 'a', 'b' }));
+
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_OnlyDigits_ReturnsEmptyArray()
+    {
+        // Arrange
+        char[] arr = new char[] { '0', '1', '2', '3' };
+
+        // Act
+        char[] result = Fake.RemoveStringNumbers(arr);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_NoDigitsInInput_ReturnsSameArray()
+    {
+        // Arrange
+        char[] arr = new char[] { 'a', 'b', 'c', 'd' };
+
+        // Act
+        char[] result = Fake.RemoveStringNumbers(arr);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(arr));
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_EmptyArray_ReturnsEmptyArray()
+    {
+        // Arrange
+        char[] arr = Array.Empty<char>();
+
+        // Act
+        char[] result = Fake.RemoveStringNumbers(arr);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+}
