f31f134b895cd3d95a559e2250317c7199a520d4
Simeon <simeonsavov21@gmail.com>
2023-10-28 14:06:22 +0300

    Add Matrix class for matrix addition
    In this commit, I have introduced the "Matrix" class, which provides a method named MatrixAddition. This method allows for the addition of two matrices represented as lists of lists of integers. The MatrixAddition method follows standard matrix addition rules, ensuring that matrices must have the same dimensions to be added.

---
+++ 15.UnitTesting_Methods_ArraysLists_Exercises/TestApp/Matrix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestApp;
+
+public class Matrix
+{
+    public static List<List<int>> MatrixAddition(
+        List<List<int>> matrixA, 
+        List<List<int>> matrixB)
+    {
+        if (matrixA.Count == 0 && matrixB.Count == 0)
+        {
+            return new();
+        }
+        else if (matrixA.Count == 0 || matrixB.Count == 0)
+        {
+            throw new ArgumentException("Matrices must have the same dimensions for addition.");
+        }
+
+        if (matrixA.Count != matrixB.Count || matrixA[0].Count != matrixB[0].Count)
+        {
+            throw new ArgumentException("Matrices must have the same dimensions for addition.");
+        }
+        else
+        {
+            for (int i = 0; i < matrixA.Count; i++)
+            {
+                if (matrixA[i].Count != matrixB[i].Count)
+                {
+                    throw new ArgumentException("Matrices must have the same dimensions for addition.");
+                }
+            }
+        }
+
+        List<List<int>> result = new();
+
+        for (int i = 0; i < matrixA.Count; i++)
+        {
+            List<int> row = new();
+
+            for (int j = 0; j < matrixA[0].Count; j++)
+            {
+                row.Add(matrixA[i][j] + matrixB[i][j]);
+            }
+
+            result.Add(row);
+        }
+
+        return result;
+    }
+}
