c1e60384f93356463b381a17afd0f6ccbbdbc622
Simeon <simeonsavov21@gmail.com>
2023-10-28 14:44:45 +0300

    Add unit tests for the Pattern class
    In this commit, a set of unit tests is added for the "Pattern" class. These tests ensure that the "SortInPattern" method functions correctly and handles various scenarios, such as sorting an array in a pattern, handling empty arrays, and preserving single-element arrays. The unit tests verify the reliability of the sorting and pattern rearrangement process within the Pattern class.

---
+++ 15.UnitTesting_Methods_ArraysLists_Exercises/TestApp.UnitTests/PatternTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class PatternTests
+{
+    [Test]
+    public void Test_SortInPattern_NullInput_ThrowsException()
+    {
+        // Arrange
+        int[]? inputArray = null;
+
+        // Act + Assert
+        Assert.That(() => Pattern.SortInPattern(inputArray), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_SortInPattern_SortsIntArrayInPattern_SortsCorrectly()
+    {
+        // Arrange
+        int[] inputArr = new int[] { 1, 2, 1, 3, 4, 10, 12, 15 };
+
+        // Act
+        int[] result = Pattern.SortInPattern(inputArr);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(new int[] { 1, 15, 2, 12, 3, 10, 4 }));
+    }
+
+    [Test]
+    public void Test_SortInPattern_EmptyArray_ReturnsEmptyArray()
+    {
+        // Arrange
+        int[] inputArr = Array.Empty<int>();
+
+        // Act
+        int[] result = Pattern.SortInPattern(inputArr);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_SortInPattern_SingleElementArray_ReturnsSameArray()
+    {
+        // Arrange
+        int[] inputArr = new int[] { 1 };
+
+        // Act
+        int[] result = Pattern.SortInPattern(inputArr);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(inputArr));
+    }
+}
