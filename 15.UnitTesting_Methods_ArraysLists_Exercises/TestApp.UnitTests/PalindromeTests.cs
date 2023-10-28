611d26362e8c022ebe490b07500b64c84bd8f497
Simeon <simeonsavov21@gmail.com>
2023-10-28 14:35:08 +0300

    Add unit tests for the Palindrome class to validate palindrome detection
    Implemented a suite of unit tests in the `PalindromeTests` class to thoroughly evaluate the `IsPalindrome` method of the `Palindrome` class. The tests cover a range of scenarios, including valid palindromes, empty input, single words, non-palindromes, and mixed-case palindromes. These tests ensure that the `IsPalindrome` method correctly identifies palindromes and non-palindromes, providing robust validation for the palindrome-checking functionality.

---
+++ 15.UnitTesting_Methods_ArraysLists_Exercises/TestApp.UnitTests/PalindromeTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class PalindromeTests
+{
+    [Test]
+    public void Test_IsPalindrome_ValidPalindrome_ReturnsTrue()
+    {
+        // Arrange
+        List<string> words = new() { "aha", "boabaob" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_EmptyList_ReturnsTrue()
+    {
+        // Arrange
+        List<string> words = new();
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_SingleWord_ReturnsTrue()
+    {
+        // Arrange
+        List<string> words = new() { "aha" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_NonPalindrome_ReturnsFalse()
+    {
+        // Arrange
+        List<string> words = new() { "haha", "aha" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Test_IsPalindrome_MixedCasePalindrome_ReturnsTrue()
+    {
+        // Arrange
+        List<string> words = new() { "Aha", "BoabAoB" };
+
+        // Act
+        bool result = Palindrome.IsPalindrome(words);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+}
