db13b2d60ccce695b62a11cf0694ed3dce474a45
Simeon <simeonsavov21@gmail.com>
2023-10-31 14:29:43 +0200

    Add Factorial Calculation and Sum of Even Digits
    In this commit, I've added code for calculating the factorial of each even digit within a given number and summing up those factorials. The program takes an integer as input and iterates through its digits, calculating the factorial of even digits and accumulating their sum. It uses the `CalculateFactorial` method to compute factorials. The result is then displayed. This code enhances the functionality by performing mathematical operations on individual digits, providing a simple yet valuable utility.

---
+++ 16.ExamPreparation_1/01.SumFactorialEvenDigits/SumFactorialEvenDigits.cs
@@ -0,0 +1,31 @@
+﻿static int CalculateFactorial(int number)
+{
+    int result = 1;
+
+    while (number > 0)
+    {
+        result *= number;
+        number -= 1;
+    }
+
+    return result;
+}
+
+
+int number = int.Parse(Console.ReadLine());
+
+int sumOfFactorials = 0;
+
+while (number != 0)
+{
+    int digit = number % 10;
+    if (digit % 2 == 0)
+    {
+        // int fact = CalculateFactorial(digit);
+        sumOfFactorials += CalculateFactorial(digit);
+    }
+
+    number /= 10;
+}
+
+Console.WriteLine(sumOfFactorials);
