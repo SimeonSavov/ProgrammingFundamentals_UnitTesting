1b9e305c96f40b93aba504dae28860f59150ac07
Simeon <simeonsavov21@gmail.com>
2023-10-31 14:36:13 +0200

    Add CenterPoint Class and Unit Tests
    In this commit, I've added the `CenterPoint` class, which contains the `GetClosest` method. The `GetClosest` method takes coordinates of two points (x1, y1, x2, y2) and determines which point is closer to the origin (0, 0). I've also included a set of unit tests for this method to validate its functionality in different scenarios. These tests cover cases where one or both points have negative coordinates, as well as situations where both points are equidistant from the origin. The tests ensure the correct behavior of the `GetClosest` method.

---
+++ 16.ExamPreparation_1/03.CenterPoint/TestApp.Tests/CenterPointTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class CenterPointTests
+{
+    [Test]
+    public void Test_GetClosest_WhenFirstPointIsCloser_ShouldReturnFirstPoint()
+    {
+        // Arrange
+        double firstNumberX1 = 1;
+        double secondNumberY1 = 1;
+        double firstNumberX2 = 2;
+        double secondNumberY2 = 2;
+
+        // Act
+        string result = CenterPoint.GetClosest(firstNumberX1, secondNumberY1, firstNumberX2, secondNumberY2);
+
+        // Assert
+        Assert.AreEqual("(1, 1)", result);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenSecondPointIsCloser_ShouldReturnSecondPoint()
+    {
+        // Arrange
+        double firstNumberX1 = 2;
+        double secondNumberY1 = 2;
+        double firstNumberX2 = 1;
+        double secondNumberY2 = 1;
+
+        // Act
+        string result = CenterPoint.GetClosest(firstNumberX1, secondNumberY1, firstNumberX2, secondNumberY2);
+
+        // Assert
+        Assert.AreEqual("(1, 1)", result);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenBothPointsHaveEqualDistance_ShouldReturnFirstPoint()
+    {
+        // Arrange
+        double firstNumberX1 = 2;
+        double secondNumberY1 = 2;
+        double firstNumberX2 = 2;
+        double secondNumberY2 = 2;
+
+        // Act
+        string result = CenterPoint.GetClosest(firstNumberX1, secondNumberY1, firstNumberX2, secondNumberY2);
+
+        // Assert
+        Assert.AreEqual("(2, 2)", result);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenFirstPointIsNegative_ShouldReturnFirstPoint()
+    {
+        // Arrange
+        double firstNumberX1 = -1;
+        double secondNumberY1 = -1;
+        double firstNumberX2 = 2;
+        double secondNumberY2 = 2;
+
+        // Act
+        string result = CenterPoint.GetClosest(firstNumberX1, secondNumberY1, firstNumberX2, secondNumberY2);
+
+        // Assert
+        Assert.AreEqual("(-1, -1)", result);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenSecondPointIsNegative_ShouldReturnSecondPoint()
+    {
+        // Arrange
+        double firstNumberX1 = 2;
+        double secondNumberY1 = 2;
+        double firstNumberX2 = -1;
+        double secondNumberY2 = -1;
+
+        // Act
+        string result = CenterPoint.GetClosest(firstNumberX1, secondNumberY1, firstNumberX2, secondNumberY2);
+
+
+        // Assert
+        Assert.AreEqual("(-1, -1)", result);
+    }
+}
