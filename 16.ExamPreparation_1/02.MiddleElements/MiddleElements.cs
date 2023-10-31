479b8214f2c3bdad7324d3b237c65f4190915bf9
Simeon <simeonsavov21@gmail.com>
2023-10-31 14:31:30 +0200

    Add Calculation of Average for Middle Elements
    In this commit, I've added code that calculates and displays the average of the middle elements of an integer array. The program takes an array of integers as input and finds the middle elements based on the array's length. It then computes the average of these middle elements and displays it with two decimal places. This code provides a straightforward method for determining the central tendency of the given data by considering the middle elements of the array.

---
+++ 16.ExamPreparation_1/02.MiddleElements/MiddleElements.cs
@@ -0,0 +1,17 @@
+﻿int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+
+int middleStart = array.Length / 2 - 1;
+int middleEnd = middleStart + 1;
+
+double sum = 0;
+
+for (int i = middleStart; i <= middleEnd; i++)
+{
+    sum += array[i];
+}
+
+double average = sum / 2;
+
+
+Console.WriteLine($"{average:f2}");
