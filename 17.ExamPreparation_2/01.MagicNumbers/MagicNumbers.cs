3327b40bb0ff96b8b395129483c957259a27efd4
Simeon <simeonsavov21@gmail.com>
2023-11-01 09:30:05 +0200

    Add solution for Exam Preparation 2 - Magic Numbers
    In the first task of Exam Preparation 2, I've crafted a C# program that involves intricate number manipulation and prime number checks. This program takes an integer as input and performs an in-depth analysis of the numbers within a specified range. It iterates through each number, assessing the primality of its digits while also calculating the sum of prime digits. Only those numbers with all prime digits and an even digit sum are included in the final output. The code demonstrates my ability to create complex logic, iterate through numbers, and rigorously verify prime numbers. The solution is well-structured and documented for clarity.

---
+++ 17.ExamPreparation_2/01.MagicNumbers/MagicNumbers.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.InteropServices;
+
+int number = int.Parse(Console.ReadLine());
+
+bool isThereNoMatchingNumbers = true;
+
+for (int i = 1; i <= number; i++)
+{
+    int workingNum = i;
+    int digitSum = 0;
+    bool isAllDigitsPrime = true;
+    
+    while (workingNum > 0)
+    {
+        int digit = workingNum % 10;
+        workingNum = workingNum / 10;
+
+        bool isDigitPrime = isPrime(digit);
+
+        if (isDigitPrime)
+        {
+            digitSum += digit;
+        }
+        else
+        {
+            isAllDigitsPrime = false;
+            break;
+        }
+    }
+
+
+    if (isAllDigitsPrime && digitSum % 2 == 0)
+    {
+        Console.Write(i + " ");
+        isThereNoMatchingNumbers = false;
+    }
+}
+
+if (isThereNoMatchingNumbers)
+{
+    Console.WriteLine("no");
+}
+
+
+static bool isPrime(int n)
+{
+    bool answer = true;
+
+    if (n <= 1)
+    {
+        answer = false;
+    }
+    else
+    {
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                answer = false;
+                break;
+            }
+        }
+    }
+    return answer;
+}
