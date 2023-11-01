b3658b04fafa921779f00ade06c050f228fd274e
Simeon <simeonsavov21@gmail.com>
2023-11-01 09:37:30 +0200

    Add solution for Exam Preparation 2 - LIS and Unit Tests
    In the fourth task of Exam Preparation 2, I've created a C# class named LongestIncreasingSubsequence. This class contains a method called GetLis, which is responsible for finding and returning the Longest Increasing Subsequence (LIS) of a given integer array. The algorithm efficiently computes the LIS and returns it as a string. To ensure the correctness and reliability of the GetLis method, comprehensive unit tests have been provided, covering various scenarios, including null arrays, empty arrays, and both unsorted and sorted arrays. This task showcases my proficiency in solving complex algorithmic problems and validating their solutions through testing.

---
+++ 17.ExamPreparation_2/04.LIS/TestApp.Tests/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class LongestIncreasingSubsequenceTests
+{
+    [Test]
+    public void Test_GetLis_NullArray_ThrowsArgumentNullException()
+    {
+        // Arrange
+        int[]? array = null;
+
+        // Act + Assert
+        Assert.That(() => LongestIncreasingSubsequence.GetLis(array), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Test_GetLis_EmptyArray_ReturnsEmptyString()
+    {
+        // Arrange
+        int[] array = new int[] { };
+
+        // Act
+        string result = LongestIncreasingSubsequence.GetLis(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(""));
+    }
+
+    [Test]
+    public void Test_GetLis_SingleElementArray_ReturnsElement()
+    {
+        // Arrange
+        int[] array = new int[] { 1 };
+
+        // Act
+        string result = LongestIncreasingSubsequence.GetLis(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("1"));
+    }
+
+    [Test]
+    public void Test_GetLis_UnsortedArray_ReturnsLongestIncreasingSubsequence()
+    {
+        // Arrange
+        int[] array = new int[] { 1, 2, 4, 5, 2, 3 };
+
+        // Act
+        string result = LongestIncreasingSubsequence.GetLis(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("1 2 4 5"));
+    }
+
+    [Test]
+    public void Test_GetLis_SortedArray_ReturnsItself()
+    {
+        // Arrange
+        int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+        // Act
+        string result = LongestIncreasingSubsequence.GetLis(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("1 2 3 4 5"));
+    }
+}
