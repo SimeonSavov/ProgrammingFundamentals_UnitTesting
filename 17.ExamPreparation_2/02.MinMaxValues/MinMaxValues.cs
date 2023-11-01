5f8dc67bb076544dfcbcbdaa89301e6f92ae24dd
Simeon <simeonsavov21@gmail.com>
2023-11-01 09:31:28 +0200

    Add solution for Exam Preparation 2 - Min Max Value
    In the second task of Exam Preparation 2, I've developed a C# program that processes an array of integers. The program first reads an array of integers and the "selection" value from the console. It then calculates the maximum and minimum values within the array based on the specified selection count. The code employs a loop to efficiently track and update these values, ensuring accuracy and efficiency. The solution showcases my ability to work with arrays and efficiently compute maximum and minimum values within a given range.

---
+++ 17.ExamPreparation_2/02.MinMaxValues/MinMaxValues.cs
@@ -0,0 +1,23 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int selection = int.Parse(Console.ReadLine());
+
+int max = int.MinValue;
+int min = int.MaxValue;
+
+for (int i = 0; i < selection; i++) // Here I only loop to the "selection" input from the console
+{
+    int currentNum = numbers[i];
+
+    if (max < currentNum)
+    {
+        max = currentNum;
+    }
+
+    if (min > currentNum)
+    {
+        min = currentNum;
+    }
+}
+
+Console.WriteLine(max);
+Console.WriteLine(min);
