6d5b0940614d45fdbe419a7643645beb39d25517
Simeon <simeonsavov21@gmail.com>
2023-11-03 20:22:35 +0200

    Add a program to calculate the average of selected elements
    In this commit, I've introduced a C# program that calculates the average of a specific number of elements from an integer array. The program prompts the user for an array and the selection value. It then iterates through the array in reverse order, summing up the selected elements and computing the average. The result is formatted to two decimal places and displayed to the console, providing an efficient tool for calculating averages from a given set of elements. The program is designed to handle various scenarios and edge cases for accurate results.

---
+++ 18.ExamPreparation_3/02.AverageLastElements/AverageLastElements.cs
@@ -0,0 +1,13 @@
+﻿int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int selection = int.Parse(Console.ReadLine());
+
+int sumOfElements = 0;
+
+for (int i = array.Length - 1; i > array.Length - 1 - selection; i--)
+{
+    int currentNum = array[i];
+    sumOfElements += currentNum;
+}
+
+double result = (double) sumOfElements / selection;
+Console.WriteLine($"{result:F2}");
