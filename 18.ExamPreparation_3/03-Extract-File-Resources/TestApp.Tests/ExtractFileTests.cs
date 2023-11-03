fa8fe08bcf8394a3ccd1b3e6c7f7b2be9f16fc1a
Simeon <simeonsavov21@gmail.com>
2023-11-03 20:24:36 +0200

    Add a program to extract file name and extension from a path
    In this commit, I've introduced a C# program that extracts the file name and extension from a given file path. The GetFile method takes a file path as input, validates it for null or empty values, and then processes it to retrieve the file name and extension. The program accounts for various scenarios, such as paths with special characters, those without extensions, and valid file paths with both file names and extensions. A set of unit tests has been implemented to validate the correctness and robustness of the extraction method, ensuring it provides accurate results even in complex scenarios.

---
+++ 18.ExamPreparation_3/03-Extract-File-Resources/TestApp.Tests/ExtractFileTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TestApp.Tests;
+
+public class ExtractFileTests
+{
+    [Test]
+    public void Test_GetFile_NullPath_ThrowsArgumentNullException()
+    {
+        // Arrange
+        string? path = null;
+
+        // Act + Assert
+        Assert.That(() => ExtractFile.GetFile(path), Throws.ArgumentNullException);
+
+    }
+
+    [Test]
+    public void Test_GetFile_EmptyPath_ThrowsArgumentNullException()
+    {
+        // Arrange
+        string path = string.Empty;
+
+        // Act + Assert
+        Assert.That(() => ExtractFile.GetFile(path), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Test_GetFile_ValidPath_ReturnsFileNameAndExtension()
+    {
+        // Arrange
+        string path = "C:\\Users\\file.txt";
+
+        string expected = "File name: file\nFile extension: txt";
+
+        // Act
+        string result = ExtractFile.GetFile(path);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithNoExtension_ReturnsFileNameOnly()
+    {
+        // Arrange
+        string input = "C:\\Users\\file";
+
+        string expected = "File name: file";
+
+        // Act
+        string result = ExtractFile.GetFile(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithSpecialCharacters_ReturnsFileNameAndExtension()
+    {
+        // Arrange
+        string input = "C:\\U%se-r.s\\file.txt";
+
+        string expected = "File name: file\nFile extension: txt";
+
+        // Act
+        string result = ExtractFile.GetFile(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
