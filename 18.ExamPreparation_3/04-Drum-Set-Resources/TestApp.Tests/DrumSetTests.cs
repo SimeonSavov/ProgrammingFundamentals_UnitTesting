83e587759b930db66dbcc98f3bcad5698aae5c9c
Simeon <simeonsavov21@gmail.com>
2023-11-03 20:26:12 +0200

    Add a program to simulate drum hits and determine the quality and amount left
    In this commit, I've introduced a C# program to simulate drum hits and calculate the quality of the drums and the amount of money left. The Drum method takes initial quality values, a list of commands that represent drum hits, and an initial amount of money as inputs. The program processes these commands, reducing the quality of the drums based on the power of the hits and deducting money for repairs or drum replacements when needed. The method is also designed to handle various edge cases, such as invalid commands and insufficient balance, ensuring the integrity and robustness of the simulation. A suite of unit tests has been implemented to validate the correctness and reliability of the program in different drumming scenarios.

---
+++ 18.ExamPreparation_3/04-Drum-Set-Resources/TestApp.Tests/DrumSetTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using System;
+
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class DrumSetTests
+{
+    [Test]
+    public void Test_Drum_TerminateCommandNotGiven_ThrowsArgumentException()
+    {
+        // Arrange
+        decimal money = 100;
+        List<int> initialQuality = new() { 2, 3 };
+        List<string> commands = new() { "1", "1" };
+
+        // Act + Assert
+        Assert.That(() => DrumSet.Drum(money, initialQuality, commands), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_Drum_StringGivenAsCommand_ThrowsArgumentException()
+    {
+        // Arrange
+        decimal money = 100;
+        List<int> initialQuality = new() { 2, 3 };
+        List<string> commands = new() { "1", "string", "Hit it again, Gabsy!" };
+
+        // Act + Assert
+        Assert.That(() => DrumSet.Drum(money, initialQuality, commands), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_Drum_ReturnsCorrectQualityAndAmount()
+    {
+        // Arrange
+        decimal money = 100;
+        List<int> initialQuality = new() { 3, 4 };
+        List<string> commands = new() { "1", "1", "Hit it again, Gabsy!" };
+
+        string expected = "1 2\nGabsy has 100.00lv.";
+
+        // Act
+        string result = DrumSet.Drum(money, initialQuality, commands);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+        
+    }
+
+    [Test]
+    public void Test_Drum_BrokenDrum_ReturnsCorrectQualityAndAmount()
+    {
+        // Arrange
+        decimal money = 100;
+        List<int> initialQuality = new() { 3, 10 };
+        List<string> commands = new() { "2", "2", "Hit it again, Gabsy!" };
+
+        string expected = "3 6\nGabsy has 91.00lv.";
+
+        // Act
+        string result = DrumSet.Drum(money, initialQuality, commands);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void Test_Drum_BalanceZero_WithOneDrumLeftOver_ReturnsCorrectQualityAndAmount()
+    {
+        // Arrange
+        decimal money = 0;
+        List<int> initialQuality = new() { 3, 4, 5 };
+        List<string> commands = new() { "2", "2", "Hit it again, Gabsy!" };
+
+        string expected = "1\nGabsy has 0.00lv.";
+
+        // Act
+        string result = DrumSet.Drum(money, initialQuality, commands);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Drum_NotEnoughBalance_RemovesAllDrums_ReturnsCorrectQualityAndAmount()
+    {
+        // Arrange
+        decimal money = 5;
+        List<int> initialQuality = new() { 3, 4, 5 };
+        List<string> commands = new() { "3", "3", "Hit it again, Gabsy!" };
+
+        string expected = "\nGabsy has 5.00lv.";
+
+        // Act
+        string result = DrumSet.Drum(money, initialQuality, commands);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
