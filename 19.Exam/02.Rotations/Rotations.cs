d75d60b145287eba79ece474e3f9560559b6d17e
Simeon <simeonsavov21@gmail.com>
2023-11-05 17:00:32 +0200

    Add solution for rotating elements in a list
    This commit introduces a solution for rotating elements within a list. The code reads an input list of integers and the number of rotations to perform. It then repeatedly moves the last element to the front of the list for the specified number of rotations. Finally, it displays the modified list.

---
+++ 19.Exam/02.Rotations/Rotations.cs
@@ -0,0 +1,11 @@
+﻿List<int> array = Console.ReadLine().Split().Select(int.Parse).ToList();
+int rotations = int.Parse(Console.ReadLine());
+
+for (int rotation = 0; rotation < rotations; rotation++)
+{
+    int lastElement = array[array.Count - 1];
+    array.RemoveAt(array.Count - 1);
+    array.Insert(0, lastElement);
+}
+
+Console.WriteLine(string.Join(", ", array));
