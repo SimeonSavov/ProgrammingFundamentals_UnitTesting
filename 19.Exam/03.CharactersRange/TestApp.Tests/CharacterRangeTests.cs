3bfa7fc50a88296fcdaf9e0bd3f11b6a81dafc0c
Simeon <simeonsavov21@gmail.com>
2023-11-05 17:04:47 +0200

    Add solution for generating character range
    This commit presents a solution for generating a range of characters between two given characters. The CharacterRange class includes a GetRange method that takes two characters, 'a' and 'b', and returns a string containing all the characters in between (exclusive). The unit tests verify the correctness of the range generation for various input scenarios.
    
    The commit provides an implementation for this character range generation task and serves as a starting point for further development and testing.

---
+++ 19.Exam/03.CharactersRange/TestApp.Tests/CharacterRangeTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class CharacterRangeTests
+{
+    [Test]
+    public void Test_GetRange_WithAAndBInOrder_ReturnsEmptyString()
+    {
+        // Arrange
+        char one = 'A';
+        char two = 'B';
+
+        // Act
+        string result = CharacterRange.GetRange(one, two);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetRange_WithBAndAInOrder_ReturnsEmptyString()
+    {
+        // Arrange
+        char one = 'B';
+        char two = 'A';
+
+        // Act
+        string result = CharacterRange.GetRange(one, two);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetRange_WithAAndCInOrder_ReturnsB()
+    {
+        // Arrange
+        char one = 'A';
+        char two = 'C';
+
+        // Act
+        string result = CharacterRange.GetRange(one, two);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("B"));
+    }
+
+    [Test]
+    public void Test_GetRange_WithDAndGInOrder_Returns_E_F()
+    {
+        // Arrange
+        char one = 'D';
+        char two = 'G';
+
+        // Act
+        string result = CharacterRange.GetRange(one, two);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("E F"));
+    }
+
+    [Test]
+    public void Test_GetRange_WithXAndZInOrder_Returns_Y()
+    {
+        // Arrange
+        char one = 'X';
+        char two = 'Z';
+
+        // Act
+        string result = CharacterRange.GetRange(one, two);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Y"));
+    }
+}
