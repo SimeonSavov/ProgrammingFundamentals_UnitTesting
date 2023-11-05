782505029498388d7f4cc9363f60eaf2a8651438
Simeon <simeonsavov21@gmail.com>
2023-11-05 16:59:13 +0200

    Add initial solution for calculating average grade
    This initial solution addresses the task of calculating the average grade for a given number of students. The code prompts the user for the number of students and their corresponding grades. It then computes the average grade by summing up all the grades and dividing by the number of students. The result is displayed with two decimal places for precision.

---
+++ 19.Exam/01.CalculateAverageGrade/CalculateAverageGrade.cs
@@ -0,0 +1,14 @@
+﻿int students = int.Parse(Console.ReadLine());
+
+double sumOfGrades = 0;
+
+for (int i = 0; i < students; i++)
+{
+    double grade = double.Parse(Console.ReadLine());
+
+    sumOfGrades += grade;
+}
+
+double result = sumOfGrades / students;
+
+Console.WriteLine($"{result:f2}");
